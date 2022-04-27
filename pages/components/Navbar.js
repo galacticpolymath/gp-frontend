3713db4122e57923bd04731b31fa2edc04690f1a
Kenzie Bottoms <kenziebottoms@gmail.com>
2022-04-27 17:55:46 -0500

    contained mobile friendly navbar

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -6,30 +6,27 @@ import Logo from '../../img/GP_logo_wordmark_horiz_grad_transBG_300.png';
 export default function Navbar() {
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-      <div className='container-fluid d-flex'>
-        <div>
-          <Link href="/" passHref>
-            <a>
-              <Image
-                className=''
-                alt="Galactic Polymath"
-                src={Logo}
-                height={35}
-                width={300} />
-            </a>
-          </Link>
-          <button
-            className='navbar-toggler'
-            type='button'
-            data-bs-toggle='collapse'
-            data-bs-target='#navbarSupportedContent'
-            aria-controls='navbarSupportedContent'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
+      <div className='container'>
+        <Link href="/" passHref>
+          <a className='flex-grow-1'>
+            <Image
+              alt="Galactic Polymath"
+              src={Logo}
+              height={35}
+              width={300} />
+          </a>
+        </Link>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
           >
-            <span className='navbar-toggler-icon'></span>
-          </button>
-        </div>
+          <span className='navbar-toggler-icon'></span>
+        </button>
         <div>
           <div className='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
