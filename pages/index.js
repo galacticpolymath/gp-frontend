f60675fb1102b9587f0dedcda32011974e58258b
Kenzie Bottoms <kenziebottoms@gmail.com>
2022-04-27 18:04:07 -0500

    hero styling

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import HeroImage from '../img/city_network.jpg';
 
 export default function Home() {
   return <Layout>
-    <div style={{ backgroundImage: `url(${HeroImage.src})` }}>
-      <div className={`container row mx-auto align-items-start ${styles.hero}`}>
-        <div className='col col-md-8 col-lg-6 py-3'>
-          <h1>We are an education studio.</h1>
-          <p>We translate current research into creative, interdisciplinary lessons for grades 5+ that are free for <em>everyone</em>.</p>
+    <div style={{ backgroundImage: `url(${HeroImage.src})` }} className={styles.hero}>
+      <div className="container row mx-auto align-items-start">
+        <div className='col col-md-8 col-lg-6'>
+          <h1 className={styles.shadow}>We are an education studio.</h1>
+          <p className={styles.shadow}>We translate current research into creative, interdisciplinary lessons for grades 5+ that are free for <em>everyone</em>.</p>
         </div>
       </div>
     </div>
