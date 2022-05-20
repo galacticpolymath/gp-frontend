96df20eb998ab802bdc1ecf7679e10600439647f
Kenzie Bottoms <kenziebottoms@gmail.com>
2022-05-19 20:33:33 -0500

    minimalist lesson cards with badges

--- pages/lessons/index.js
+++ pages/lessons/index.js
@@ -1,11 +1,37 @@
+import Hero from '../components/Hero';
 import Layout from '../components/Layout';
 
-const LessonsPage = () => {
+const LessonsPage = ({ lessons }) => {
   return (
     <Layout>
-      <h1>Lessons</h1>
+      <Hero className="bg-secondary">
+        <h1>Interdisciplinary Lessons</h1>
+        <p>Our lessons are free. We strive to create mind-expanding learning experiences that a non-specialist can teach in <em>any G5-12 classroom</em> with 15 minutes of prep time!</p>
+      </Hero>
+
+      <div className='bg-light-gray'>
+        <div className='container mx-auto grid py-5 px-3 gap-3'>
+          {lessons.map(lesson => (
+            <div key={lesson.id} className='bg-white rounded-3 g-col-6 p-3'>
+              <h3 className='fw-light'>{lesson.Title}</h3>
+              <p>{lesson.Subtitle}</p>
+              <span className={`badge bg-${lesson.Section.overview.TargetSubject.toLowerCase().replace(/\s/g, ' ')}`}>
+                {lesson.Section.overview.TargetSubject}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
     </Layout>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://catalog.galacticpolymath.com/index.json');
+
+  const lessons = await res.json();
+
+  return { props: { lessons } };
 };
 
 export default LessonsPage;
