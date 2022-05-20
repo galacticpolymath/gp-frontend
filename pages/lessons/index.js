4291254156abb2bd3966963344a1d225e7c11765
Kenzie Bottoms <kenziebottoms@gmail.com>
2022-05-19 20:52:15 -0500

    finish cards

--- pages/lessons/index.js
+++ pages/lessons/index.js
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 import Hero from '../components/Hero';
 import Layout from '../components/Layout';
 
@@ -11,14 +14,33 @@ const LessonsPage = ({ lessons }) => {
 
       <div className='bg-light-gray'>
         <div className='container mx-auto grid py-5 px-3 gap-3'>
+          {/* TODO: hide unpublished lessons? */}
           {lessons.map(lesson => (
-            <div key={lesson.id} className='bg-white rounded-3 g-col-6 p-3'>
-              <h3 className='fw-light'>{lesson.Title}</h3>
-              <p>{lesson.Subtitle}</p>
-              <span className={`badge bg-${lesson.Section.overview.TargetSubject.toLowerCase().replace(/\s/g, ' ')}`}>
-                {lesson.Section.overview.TargetSubject}
-              </span>
-            </div>
+            <Link
+              key={lesson.id}
+              href={`/lessons/${ lesson.id}`}
+              passHref
+            >
+              <a className='d-block bg-white rounded-3 g-col-6 no-hover-color-change'>
+                <div>
+                  <Image
+                    src={lesson.CoverImage.url}
+                    alt={lesson.Subtitle}
+                    layout="responsive"
+                    // TODO: will these always be the same size?
+                    width={1500}
+                    height={450}
+                  />
+                </div>
+                <div className='p-3'>
+                  <h3 className='fw-light'>{lesson.Title}</h3>
+                  <p>{lesson.Subtitle}</p>
+                  <span className={`badge bg-${lesson.Section.overview.TargetSubject.toLowerCase().replace(/\s/g, ' ')}`}>
+                    {lesson.Section.overview.TargetSubject}
+                  </span>
+                </div>
+              </a>
+            </Link>
           ))}
         </div>
       </div>
