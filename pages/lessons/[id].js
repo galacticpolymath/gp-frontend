0545285e1deae012de5f158e4b90b945ac34009c
Kenzie Bottoms <kenziebottoms@gmail.com>
2022-05-29 19:28:33 -0500

    lesson header

--- pages/lessons/[id].js
+++ pages/lessons/[id].js
@@ -1,9 +1,40 @@
+import Image from 'next/image';
+
 import Layout from '../components/Layout';
+import RichText from '../components/RichText';
 
 const LessonDetails = ({ lesson }) => {
   return (
     <Layout>
-      <h1>{lesson.Title}</h1>
+      <div className="bg-light-gray p-5">
+        <div className="container">
+          <h1>{lesson.Title}</h1>
+          <h4 className='fw-light'>{lesson.Subtitle}</h4>
+          <Image
+            src={lesson.CoverImage.url}
+            alt={lesson.Subtitle}
+            layout="responsive"
+            // TODO: will these always be the same size?
+            width={1500}
+            height={450}
+          />
+          <div className='row mt-4'>
+            <div className="col col-md-6 col-lg-9">
+              <h5>Sponsored by:</h5>
+              <RichText content={lesson.SponsoredBy} />
+            </div>
+            <div className="col col-md-6 col-lg-3">
+              <Image
+                src={lesson.SponsorImage.url}
+                alt={lesson.Subtitle}
+                layout="responsive"
+                width={1}
+                height={1}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
