0d94501f116233b5b35c6a6f706738e8ac81eb8c
Kenzie Bottoms <kenziebottoms@gmail.com>
2022-05-29 23:29:12 -0500

    fix hero image

--- components/Hero.js
+++ components/Hero.js
@@ -5,10 +5,10 @@ import styles from './Hero.module.css';
 const Hero = ({
   imgSrc,
   children,
-  className,
+  className = '',
 }) => {
   return (
-    <div style={imgSrc ? {} : { backgroundImage: `url(${imgSrc})` }} className={`${styles.hero} ${className}`}>
+    <div style={imgSrc ? { backgroundImage: `url(${imgSrc})` } : {}} className={`${styles.hero} ${className}`}>
       <div className="container row mx-auto align-items-start">
         <div className='col col-md-8 col-lg-6'>
           {children}
