02ea74aa7f8f485f4357691337140e817344721f
Kenzie Bottoms <kenziebottoms@gmail.com>
2022-07-05 20:49:01 -0500

    extract accordion

---
+++ components/Accordion.js
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+
+import styles from './Accordion.module.scss';
+
+const Accordion = ({
+  id,
+  title = '',
+  className = '',
+  children,
+  initiallyExpanded = false,
+}) => {
+  return (
+    <div className={`accordion ${className} ${styles.Accordion}`}>
+      <div className="accordion-item">
+        <h2 className="accordion-header" id={`heading_${id}`}>
+          <button
+            className={`accordion-button ${initiallyExpanded ? '' : 'collapsed'}`}
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target={`#content_${id}`}
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            <div className='container mx-auto text-black'>
+              <h3 className='m-0'>{title}</h3>
+              TODO: arrow
+            </div>
+          </button>
+        </h2>
+        <div
+          id={`content_${id}`}
+          className={`accordion-collapse collapse ${initiallyExpanded ? 'show' : ''}`}
+          aria-labelledby={`${id}_content`}
+          data-bs-parent="#accordionExample"
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Accordion.propTypes = {
+  id: PropTypes.string.isRequired,
+  SectionTitle: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.object,
+  initiallyExpanded: PropTypes.bool,
+};
+
+export default Accordion;
--- components/CollapsibleLessonSection.js
+++ components/CollapsibleLessonSection.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import styles from './CollapsibleLessonSection.module.scss';
+import Accordion from './Accordion';
 
 const CollapsibleLessonSection = ({
   index,
@@ -11,33 +11,15 @@ const CollapsibleLessonSection = ({
 }) => {
   const accordionId = SectionTitle.replace(/[\s!]/gi, '_').toLowerCase();
   return (
-    <div className={`accordion ${className} ${styles.CollapsibleLessonSection}`}>
-      <div className="accordion-item">
-        <h2 className="accordion-header" id={`heading_${accordionId}`}>
-          <button
-            className={`accordion-button ${initiallyExpanded ? '' : 'collapsed'}`}
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target={`#content_${accordionId}`}
-            aria-expanded="true"
-            aria-controls="collapseOne"
-          >
-            <div className='container mx-auto text-black'>
-              <h3 className='m-0'>{index && `${index}. `}{SectionTitle}</h3>
-              {/* TODO: arrow */}
-            </div>
-          </button>
-        </h2>
-        <div
-          id={`content_${accordionId}`}
-          className={`accordion-collapse collapse ${initiallyExpanded ? 'show' : ''}`}
-          aria-labelledby={`${accordionId}_content`}
-          data-bs-parent="#accordionExample"
-        >
-          {children}
-        </div>
-      </div>
-    </div>
+    <Accordion
+      initiallyExpanded={initiallyExpanded}
+      id={accordionId}
+      title={`${index && `${index}. `}${SectionTitle}`}
+      className={className}
+      buttonClassName='accordion-button'
+    >
+      {children}
+    </Accordion>
   );
 };
 
