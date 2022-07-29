870488aad9f5a2df3d9a2c6251af24ec11b5bba2
Kenzie Bottoms <kenziebottoms@gmail.com>
2022-07-29 17:19:04 -0500

    collapsible text section

---
+++ components/LessonSection/CollapsibleRichTextSection.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import CollapsibleLessonSection from '../CollapsibleLessonSection';
+import RichText from '../RichText';
+
+const CollapsibleRichTextSection = ({
+  Content,
+  InitiallyExpanded,
+  ...props
+}) => (
+  <CollapsibleLessonSection initiallyExpanded={InitiallyExpanded} {...props}>
+    <div className='container mx-auto mb-4'>
+      <RichText className='mt-4' content={Content} />
+    </div>
+  </CollapsibleLessonSection>
+);
+
+CollapsibleRichTextSection.propTypes = {
+  Content: PropTypes.string,
+};
+
+export default CollapsibleRichTextSection;
