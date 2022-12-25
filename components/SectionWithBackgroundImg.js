a810942f3caa8fc007074832823b8410b6d71ab0
Gabe <gtorion97@gmail.com>
2022-12-25 13:21:47 -0600

    refactoring background of feedback and review container

--- components/SectionWithBackgroundImg.js
+++ components/SectionWithBackgroundImg.js
@@ -6,10 +6,9 @@
 /* eslint-disable object-curly-spacing */
 /* eslint-disable indent */
 /* eslint-disable react/jsx-indent */
-import Image from 'next/image';
 
 const SectionWithBackgroundImg = ({ children, backgroundImgSrc }) => {
-    const _classNames = `img-background-container ${!backgroundImgSrc ? 'bg-carousel-color pt-4 pb-4' : 'pt-5 pb-5 parallax '}`;
+    const _classNames = `img-background-container ${!backgroundImgSrc ? 'bg-carousel-color pt-4 pb-4' : 'pt-3 pb-5 parallax'}`;
     const _style = backgroundImgSrc ? { backgroundImage: `url(${backgroundImgSrc})` } : null;
 
     return (
