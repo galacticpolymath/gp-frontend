a4e0db8d00367abd491cdd36a729979eeac96a07
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-01-13 15:35:37 -0600

    clean up materialset, remove flow from it

--- components/LessonSection/TeachIt/MaterialSet.js
+++ components/LessonSection/TeachIt/MaterialSet.js
@@ -3,63 +3,61 @@ import PropTypes from 'prop-types';
 import Accordion from '../../Accordion';
 
 const MaterialSet = ({
-  materialSet,
+  materialSet: {
+    links,
+    parts = [],
+  },
 }) => {
   return (
     <div className='container mx-auto mb-4'>
         
-      {materialSet.links && (
+      {links && (
         <div className='text-center'>
           <a
             target='_blank'
             rel='noopener noreferrer'
-            href={materialSet.links.url}
+            href={links.url}
             className='btn btn-primary px-3 py-2 d-inline-block mb-3'
           >
             <div className='d-flex align-items-center gap-2'>
               <i className="bi-cloud-arrow-down-fill fs-3 lh-1"></i>{' '}
-              {materialSet.links.linkText}
+              {links.linkText}
             </div>
           </a>
         </div>
       )}
 
-      {materialSet.parts && materialSet.parts
-        .map(part => (
-          <Accordion
-            buttonClassName='w-100 text-start'
-            key={part.part}
-            id={`teachit_env_${part.part}`}
-            button={(
-              <div>
-                <h6>Part {part.part}: {part.title}</h6>
-                <div>{part.preface}</div>
-              </div>
-            )}
-          >
-            <>
-              <ol className='mt-3'>
-                {part.itemList && part.itemList.map(item => (
-                  <li key={item.itemTitle} className='mb-2'>
-                    <strong>{item.itemTitle}</strong>
-                    <ul>
-                      {/* TODO: DATA: always want an array */}
-                      {item.links && (Array.isArray(item.links) ? item.links : [item.links]).map((link, i) => (
-                        <li key={i}>
-                          <a href={link.url}>
-                            {link.linkText}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-                ))}
-              </ol>
-              <h5>Steps and Flow</h5>
-            </>
-          </Accordion>
-        ))
-      }
+      {parts.map(part => (
+        <Accordion
+          buttonClassName='w-100 text-start'
+          key={part.part}
+          id={`teachit_env_${part.part}`}
+          button={(
+            <div>
+              <h6>Part {part.part}: {part.title}</h6>
+              <div>{part.preface}</div>
+            </div>
+          )}
+        >
+          <ol className='mt-3'>
+            {part.itemList && part.itemList.map(item => (
+              <li key={item.itemTitle} className='mb-2'>
+                <strong>{item.itemTitle}</strong>
+                <ul>
+                  {/* TODO: DATA: always want an array */}
+                  {item.links && (Array.isArray(item.links) ? item.links : [item.links]).map((link, i) => (
+                    <li key={i}>
+                      <a href={link.url}>
+                        {link.linkText}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ol>
+        </Accordion>
+      ))}
     </div>
   );
 };
