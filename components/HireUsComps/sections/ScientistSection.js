247ce2be66a0f8ac2d95f32414cbc61bd36a7d08
Gabe <gtorion97@gmail.com>
2023-02-03 12:47:56 -0600

    changing the font size of 'How much does it cost' section

--- components/HireUsComps/sections/ScientistSection.js
+++ components/HireUsComps/sections/ScientistSection.js
@@ -20,36 +20,35 @@
 import scientists from '../../../data/HireUsPg/scientists.json'
 import PicAndDescriptionSec from './PicAndDescriptionSec';
 
-const ScientistSection = () => {
-    return (
-        <>
-            <section className="d-block">
-                <section className="w-100 d-flex justify-content-center align-items-center justify-content-md-start align-items-md-stretch mt-5 mb-5 mb-md-3 mb-xl-5 ps-md-5 pe-md-5 ms-md-5 me-md-5 pt-md-5">
-                    <h5 className="scientistHeadingTxt fw200 fst-italic text-dark text-center text-sm-start w-75">
-                        Some of the many talented scientists, communicators, educators, and artists we work with:
-                    </h5>
-                </section>
+const ScientistSection = () => (
+    <section className="scientistSection">
+        <section className="d-block">
+            <section className="w-100 d-flex justify-content-center align-items-center justify-content-md-start align-items-md-stretch mt-5 mb-5 mb-md-3 mb-xl-5 ps-md-5 pe-md-5 ms-md-5 me-md-5 pt-md-5">
+                <h5 className="scientistHeadingTxt fw200 fst-italic text-dark text-center text-sm-start w-75">
+                    Some of the many talented scientists, communicators, educators, and artists we work with:
+                </h5>
             </section>
-            <section className="d-flex justify-content-center align-items-center justify-content-md-start align-items-md-stretch ps-md-5 pe-md-5 ms-md-5 me-md-5 pb-md-5 pb-xl-5">
-                <section className="d-flex d-md-block flex-column flex-md-row justify-content-center justify-content-md-start scientistSec">
-                    {scientists.map((scientist, index) => {
-                        const { alt, src, name, description } = scientist;
+        </section>
+        <section className="d-flex justify-content-center align-items-center justify-content-md-start align-items-md-stretch ps-md-5 pe-md-5 ms-md-5 me-md-5 pb-md-5 pb-xl-5">
+            <section className="d-flex d-md-block flex-column flex-md-row justify-content-center justify-content-md-start scientistSec">
+                {scientists.map((scientist, index) => {
+                    const { alt, src, name, description } = scientist;
 
-                        return (
-                            <PicAndDescriptionSec
-                                key={index}
-                                imgPath={src}
-                                text={description}
-                                name={name}
-                                alt={alt}
-                                parentSecStyles="secWithHumanPic scientist d-md-inline-flex"
-                            />
-                        )
-                    })}
-                </section>
+                    return (
+                        <PicAndDescriptionSec
+                            key={index}
+                            imgPath={src}
+                            text={description}
+                            name={name}
+                            alt={alt}
+                            parentSecStyles="secWithHumanPic scientist d-md-inline-flex"
+                        />
+                    )
+                })}
             </section>
-        </>
-    )
-}
+        </section>
+    </section>
+)
+
 
 export default ScientistSection;
