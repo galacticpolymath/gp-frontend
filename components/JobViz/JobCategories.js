74cab80a7ed5321ec7ae1f7e9dad02848e04312c
Gabe <gtorion97@gmail.com>
2023-02-15 15:21:51 -0600

    getting the results when the user clicks on 'more jobs' button for 'Architecture & Engineering occupations' card

--- components/JobViz/JobCategories.js
+++ components/JobViz/JobCategories.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-max-props-per-line */
+/* eslint-disable comma-dangle */
 /* eslint-disable quotes */
 /* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable react/jsx-indent */
@@ -8,12 +10,54 @@ import { Button } from "react-bootstrap";
 import AccountTreeIcon from "@material-ui/icons/AccountTree";
 
 // if there are no results, then display the default results (see the current ui)
+const startingResults =
+    [
+        {
+            jobField: "Architecture & Engineering occupations",
+            id: "engineeringElId",
+            path: "17-0000"
+        },
+        {
+            jobField: "Arts, design, entertainment, sports, & media occupations",
+            id: "artsDesignElId",
+            startingLevel: "27-0000"
+        },
+        {
+            jobField: "Building & grounds cleaning & maintenance occupations",
+            id: "maintenanceElId",
+            startingLevel: "37-0000"
+        },
+        {
+            jobField: "Business & financial operations occupations",
+            id: "businessElId",
+            startingLevel: "13-0000"
+        }
+        ,
+        {
+            jobField: "Community & social service occupations",
+            id: "serviceElId",
+            startingLevel: "21-0000"
+        },
+        {
+            jobField: "Computer & mathematical occupations",
+            id: "computerElId",
+            startingLevel: "15-0000"
+        }
+    ]
 
+// CASE:
+// the user clicks on the "More Jobs" button for "Architecture & Engineering occupations"
+// GOAL:
+// get all of data from jobVizData that meets the following criteria: 
+// hierarchy = 2
+// level1 = 17-0000
 const JobCategories = ({ searchResults }) => {
     // put the path into the url:
-    const startingResults = [{ jobField: "Architecture & Engineering occupations", path: "" }, { jobField: "Arts, design, entertainment, sports, & media occupations", path: "" }, { jobField: "Building & grounds cleaning & maintenance occupations", path: "" }, { jobField: "Business & financial operations occupations", path: "" }, { jobField: "Community & social service occupations", path: "" }, { jobField: "Computer & mathematical occupations", path: "" }]
+
 
     // when the user clicks on any of the buttons in the above, take the user to specific search result. 
+
+    // When the user clicks on 
 
     return (
         <section className="pt-5 d-flex justify-content-center align-items-center">
@@ -24,8 +68,8 @@ const JobCategories = ({ searchResults }) => {
                 :
                 <div className="jobCategoriesSec">
                     {/* display the default search results */}
-                    {startingResults.map(({ jobField }, key) =>
-                        <div key={key} className="shadow jobFieldStartingResult d-flex flex-column">
+                    {startingResults.map(({ jobField, id }) =>
+                        <div id={id} key={id} className="shadow jobFieldStartingResult d-flex flex-column">
                             <section className="w-100 h-50 d-flex justify-content-center align-items-center">
                                 <h4 className="text-center">{jobField}</h4>
                             </section>
--- pages/api/jobVizData.js
+++ pages/api/jobVizData.js
@@ -1,12 +1,24 @@
+/* eslint-disable no-console */
 import path from 'path';
 import { promises as fileSystem } from 'fs';
 
+// create a function that will get all of the elements depending on the hierarch level
+// filter based on the following criteria:
+// level 1 === target hierarchy level
+// hierarchy number === target hierarchy number
+// show the details of the specific field in the top card 
+
 export default async function handler(req, res) {
   //Find the absolute path of the json directory
+  console.log('req: ', req);
+
   const jsonDirectory = path.join(process.cwd(), 'data/Jobviz');
   //Read the json data file data.json
   const targetFile = `${jsonDirectory}/jobVizData.json`;
   const fileContents = await fileSystem.readFile(targetFile, 'utf8');
+  console.log('fileContents: ', fileContents);
+  
   //Return the content of the data file in json format
+  console.log('res: ', res);
   res.status(200).json(fileContents);
 }
