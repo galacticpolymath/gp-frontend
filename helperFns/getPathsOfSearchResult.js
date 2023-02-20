a0ae1f8e7d9840164366fb6f5eb3ebe90f329fd9
Gabe <gtorion97@gmail.com>
2023-02-19 19:07:37 -0600

    fixing bugs for 'getPathsOfSearchResult'

--- helperFns/getPathsOfSearchResult.js
+++ helperFns/getPathsOfSearchResult.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-empty */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-debugger */
 /* eslint-disable no-multiple-empty-lines */
@@ -8,12 +9,91 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable indent */
 
+const jobVizData = require("../data/Jobviz/jobVizData.json");
+
+const getFirstParentJobCategory = jobCategories => jobCategories.reduce((firstParentJobCategory, jobCategory) => {
+    return (jobCategory?.hierarchy > firstParentJobCategory.hierarchy) ? jobCategory : firstParentJobCategory;
+});
+
+const getAllParentJobCategories = (jobCategory, _num) => {
+    let targetLevels = [];
+    let num = _num;
+
+    while (num != 0) {
+        const levelN = jobCategory[`level${num}`];
+        const parentJobCategoryAtNLvl = jobVizData.find(({ soc_code, occupation_type }) => (soc_code === levelN) && (occupation_type === "Summary"));
+        targetLevels.push(parentJobCategoryAtNLvl)
+        num--;
+    }
+
+    return targetLevels;
+}
+
 
 const getPathsOfSearchResult = jobCategory => {
-    
+
+    // if(hierarchyNum === 4){   
+    let targetLevels = getAllParentJobCategories(jobCategory, (jobCategory.hierarchy === 1) ? jobCategory.hierarchy : (jobCategory.hierarchy - 1));
+    const firstParentJobCategory = getFirstParentJobCategory(targetLevels);
+    const { hierarchy, soc_code } = firstParentJobCategory
+    console.log("hierarchy: ", hierarchy)
+    targetLevels = targetLevels.sort((levelA, levelB) => levelA.hierarchy - levelB.hierarchy);
+
+    return `/${hierarchy + 1}/${soc_code}/${targetLevels.map(({ id }) => id).join('/')}`
+    // }
+
+
+
     // BRAIN DUMP:
     // get the hierarchy number for the specific job category 
     // need to get the previous job results
+    // we get the job category object from the jobVizData file
+
+
+
+    // CASE: the user goes to a line item in hierarchy level 4 
+    // the following will be pushed into the url: currentHierarchy/source_code/level2/level3/level4
+    // currentHierarchy/source_code/level2/level3/level4 will pushed into the url
+    // the levelN id has been attained
+    // target object with levelN id has been attained
+    // using jobCategory.levelN and occupation_type === "Summary"
+    // get the parent job category summary levels, by doing the above:
+    // get the latest parent job category level, get its source_code (this is the parent level) and its (hierarchy level + 1) == currentHierarchy  (the object with the highest number for the 'hierarchy' field)
+    // a line item was clicked for on the job search 
+
+    // if ((hierarchy === 4) && (occupation_type === "Line item")) {
+    //     let targetLevels = getAllParentJobCategories(jobCategory, 3); 
+    //     const firstParentJobCategory = getFirstParentJobCategory(targetLevels);
+    //     const { hierarchy, soc_code } = firstParentJobCategory
+    //     targetLevels = targetLevels.sort((levelA, levelB) => levelA.hierarchy - levelB.hierarchy);
+
+    //     return `/${hierarchy + 1}/${soc_code}/${targetLevels.map(({ id }) => id).join('/')}`
+
+    // }
+
+
+
+
+
+
+
+    // CASE: the user goes to a summary of a level 4 job categories 
+
+
+
+    // CASE: the user goes to a summary of level 3 job categories 
+
+
+
+    // CASE: the user goes to a line item in level 3 job categories 
+
+
+
+    // CASE: the user goes to a line item in level 2 job categories 
+
+
+
+    // CASE: the user goes to a summary of level 2 job categories 
 }
 
 module.exports = getPathsOfSearchResult
--- unit-testing/searchInput/getPathOfSearchResult.test.js
+++ unit-testing/searchInput/getPathOfSearchResult.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable indent */
 /* eslint-disable semi */
@@ -6,14 +7,40 @@
 /* eslint-disable no-multiple-empty-lines */
 
 const getPathsOfSearchResult = require("../../helperFns/getPathsOfSearchResult")
+const jobVizData = require("../../data/Jobviz/jobVizData.json")
 
-// when the user clicks on LOGGING WORKERS, the following should be returned from the getPathsOfSearchResults fn:
-// return val: /job-viz/4/45-4020/742/756/758
+// line item at level 4
+const selectedJobCategoryTest1 = jobVizData.find(({ id }) => id === 759);
+// summary item at level 2
+const selectedJobCategoryTest2 = jobVizData.find(({ id }) => id === 558);
+// line item at level 2
+const selectedJobCategoryTest3 = jobVizData.find(({ id }) => id === 1009);
+// third level summary item
+const selectedJobCategoryTest4 = jobVizData.find(({ id }) => id === 490);
 
-const result1 = "/job-viz/4/45-4020/742/756/758"
+const test1CorrectResult = '/4/45-4020/742/756/758'
+const test2CorrectResult = '/2/35-0000/558'
+const test3CorrectResult = '/2/51-0000/907'
+const test4CorrectResult = '/3/29-9000/412/490'
+
+
 
 test("Get the path of the selected search result.", () => {
-    const newPaths = getPathsOfSearchResult();
+    // const newPathsTest1 = getPathsOfSearchResult(selectedJobCategoryTest1);
 
-    expect(newPaths).toBe(result1)    
+    // expect(newPathsTest1).toBe(test1CorrectResult)    
+
+    // console.log("newPathsTest1 has passed.")
+
+    // const newPathsTest2 = getPathsOfSearchResult(selectedJobCategoryTest2);
+
+    // expect(newPathsTest2).toBe(test2CorrectResult)  
+
+    // const newPathsTest3 = getPathsOfSearchResult(selectedJobCategoryTest3);
+
+    // expect(newPathsTest3).toBe(test3CorrectResult)  
+
+    const newPathsTest4 = getPathsOfSearchResult(selectedJobCategoryTest4);
+
+    expect(newPathsTest4).toBe(test4CorrectResult)  
 })
