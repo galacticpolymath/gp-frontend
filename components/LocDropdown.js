2ef314c0ca5282d3f06c28e30d6f390af1208719
Gabe <gtorion97@gmail.com>
2023-03-12 19:24:35 -0500

    adding cookies, wip

--- components/LocDropdown.js
+++ components/LocDropdown.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-console */
 import React from 'react';
 import ReactFlagsSelect from 'react-flags-select';
 
 const locToCountry = { 'en-US': 'US', 'en-GB': 'GB', 'en-NZ': 'NZ', 'fr': 'FR', 'de': 'DE', 'it': 'IT', 'fr-AW': 'AW' };
 const countryToLoc = { 'US': 'en-US', 'GB': 'en-GB', 'NZ': 'en-NZ', 'FR': 'fr', 'DE': 'de', 'IT': 'it', 'AW': 'fr-AW' };
+const countriesByLanguage = [{ language: 'English', countries: ['US', 'GB', 'NZ'] }, { language: 'French', countries: ['FR', 'AW'] }, { language: 'German', countries: ['DE'] }, { language: 'Italian', countries: ['IT'] }];
 
 const LocDropdown = ({ id, availLocs, loc }) => {
 
@@ -18,6 +20,14 @@ const LocDropdown = ({ id, availLocs, loc }) => {
     const locDest = countryToLoc[country];
     window.location = `/lessons/${locDest}/${id}`;
   };
+
+  if (countries.length === 1) {
+    const { language } = countriesByLanguage.find(({ countries }) => countries.includes(countries[0]));
+    
+    return <div>{language} ({countries[0]})</div>;
+  }
+
+  // if the country is an english country, then show the following on the ui: English (abbreviation of the country)
 
   // const flag = <Flag name={loc} />
   // i can't get the flag to show up in the middle of a string
