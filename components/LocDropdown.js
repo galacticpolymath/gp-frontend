f9315abe8c4d25eabcd8a3b70109725299ab881f
Gabe <gtorion97@gmail.com>
2023-03-12 20:45:00 -0500

    added tooltip modal

--- components/LocDropdown.js
+++ components/LocDropdown.js
@@ -1,12 +1,18 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import ReactFlagsSelect from 'react-flags-select';
+import Fade from './Fade';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const locToCountry = { 'en-US': 'US', 'en-GB': 'GB', 'en-NZ': 'NZ', 'fr': 'FR', 'de': 'DE', 'it': 'IT', 'fr-AW': 'AW' };
 const countryToLoc = { 'US': 'en-US', 'GB': 'en-GB', 'NZ': 'en-NZ', 'FR': 'fr', 'DE': 'de', 'IT': 'it', 'AW': 'fr-AW' };
 const countriesByLanguage = [{ language: 'English', countries: ['US', 'GB', 'NZ'] }, { language: 'French', countries: ['FR', 'AW'] }, { language: 'German', countries: ['DE'] }, { language: 'Italian', countries: ['IT'] }];
 
 const LocDropdown = ({ id, availLocs, loc }) => {
+  const [isToolTipModalOn, setIsToolTipModalOn] = useState(true);
+  const [timer, setTimer] = useState(null);
 
   let countries = [];
   let labels = {};
@@ -21,6 +27,22 @@ const LocDropdown = ({ id, availLocs, loc }) => {
     window.location = `/lessons/${locDest}/${id}`;
   };
 
+  const handleCloseModal = () => {
+    setIsToolTipModalOn(false);
+    clearTimeout(timer);
+  };
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setIsToolTipModalOn(false);
+    }, 3500);
+    setTimer(timer);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   if (countries.length === 1) {
     const { language } = countriesByLanguage.find(({ countries }) => countries.includes(countries[0]));
 
@@ -34,6 +56,20 @@ const LocDropdown = ({ id, availLocs, loc }) => {
 
   return (
     <div className="position-relative">
+      <Fade containerId='clickMoreLocalesId' showElement={isToolTipModalOn}>
+        <div className="position-absolute toolTipModal">
+          <div className="w-100 h-100 position-relative">
+            <button className='position-absolute noBtnStyles closeToolTipModalBtn' onClick={handleCloseModal}>
+              <AiOutlineClose />
+            </button>
+            <div className="w-100 h-100 toolTipModalWrapper">
+              <h5>
+                Click for more locales!
+              </h5>
+            </div>
+          </div>
+        </div>
+      </Fade>
       <ReactFlagsSelect
         selected={loc}
         countries={countries}
