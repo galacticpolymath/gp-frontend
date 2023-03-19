60156a51cea6579af14b09dfacda5052596a41cf
Gabe <gtorion97@gmail.com>
2023-03-19 18:28:54 -0500

    fixing responsivesness for graphs, wip

--- components/LessonSection/Preview/index.js
+++ components/LessonSection/Preview/index.js
@@ -12,13 +12,15 @@ const Preview = ({
   Multimedia,
   QuickPrep,
 }) => {
+  // d-flex d-sm-block flex-column flex-sm-row justify-content-sm-start justify-content-center align-items-center align-items-sm-start
   return (
     <CollapsibleLessonSection
       index={index}
       SectionTitle={SectionTitle}
       initiallyExpanded={InitiallyExpanded !== false}
     >
-      <div className='container row mx-auto pb-4'>
+    
+      <div className='container row mx-auto pb-4 justify-content-center justify-content-sm-start'>
         <div className="col col-md-8 offset-md-2">
           <div className='bg-primary-light p-4 pb-2 fs-5 my-4 fw-light'>
             <h4>&quot;Teach it in 15&quot; Quick Prep</h4>
