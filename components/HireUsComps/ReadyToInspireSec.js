b4474e597e024a22a2f18e5168faef5944ad989f
Gabe <gtorion97@gmail.com>
2023-03-22 14:35:35 -0500

    making converting img to Image component

--- components/HireUsComps/ReadyToInspireSec.js
+++ components/HireUsComps/ReadyToInspireSec.js
@@ -13,20 +13,20 @@ const ReadyToInspireSec = () => {
             contentClassName='classRoom2ContentStyles position-relative'
           />
         </section>
-        <section className="readyToInspireTxtsSec pt-md-2 pt-sm-0 pt-4 d-md-flex justify-content-md-center align-items-md-center flex-md-column">
+        <section className="readyToInspireTxtsSec pt-md-2 pt-sm-0 pt-4 d-md-flex justify-content-md-center align-items-md-center flex-md-column pe-lg-3 pe-xl-0">
           <section className="ps-sm-5 ps-md-0 mt-3 mt-sm-4 mt-md-2 mt-lg-0 d-flex flex-column readyInspireSec">
             <span className="fs-24 d-block fw700 d-none d-sm-inline d-md-none">Ready to inspire students with</span>
             <span className="fs-24 d-block fw700 d-none d-sm-inline d-md-none">your work?</span>
-            <span className="fs-24 d-block fw700 d-none d-md-inline-flex justify-content-md-center align-items-md-center text-center d-lg-inline">Ready to inspire students with your work?</span>
-            <span className="fs-24 d-block fw700 d-inline d-sm-none text-center ps-2 pe-2">Ready to inspire students with your work?</span>
+            <span className="fs-24 d-block fw700 d-none d-md-inline-flex justify-content-md-center align-items-md-center text-center text-lg-start d-lg-inline">Ready to inspire students with your work?</span>
+            <span className="fs-24 d-block fw700 d-inline d-sm-none text-center text-lg-start ps-2 pe-2">Ready to inspire students with your work?</span>
           </section>
           <section className="ps-sm-5 ps-md-0 mt-4 d-flex flex-column orderLaCarteSec">
-            <span className="fs-med d-none fw249 d-sm-inline d-md-inline-flex justify-content-md-center align-items-md-center d-lg-none">Order <span className="fst-italic">à la carte</span> or customize a package.</span>
-            <span className="fs-med d-none fw249 d-sm-inline d-md-inline-flex justify-content-md-center align-items-md-center d-lg-none">Let us know how we can help!</span>
-            <span className="fs-med fw249 d-none d-lg-inline text-center ps-5 ps-sm-0 pe-5 pe-sm-0">Order <span className="fst-italic">à la carte</span> or customize a package. Let us know how we can help!</span>
-            <span className="fs-med d-block fw249 d-sm-none text-center ps-5 ps-sm-0 pe-5 pe-sm-0">Let us know how we can help! Order <span className="fst-italic">à la carte</span> or customize a package.</span>
+            <span className="d-none fw249 d-sm-inline d-md-inline-flex justify-content-md-center text-md-center text-lg-start align-items-md-center d-lg-none">Order <span className="fst-italic mx-md-2 mx-lg-0">à la carte</span> or customize a package.</span>
+            <span className="d-none fw249 d-sm-inline d-md-inline-flex justify-content-md-center align-items-md-center d-lg-none">Let us know how we can help!</span>
+            <span className="fw249 d-none d-lg-inline text-center text-lg-start ps-5 ps-sm-0 pe-5 pe-sm-0">Order <span className="fst-italic">à la carte</span> or customize a package. Let us know how we can help!</span>
+            <span className="d-block fw249 d-sm-none text-center text-lg-start ps-5 ps-sm-0 pe-5 pe-sm-0">Let us know how we can help! Order <span className="fst-italic">à la carte</span> or customize a package.</span>
           </section>
-          <section className="d-none d-lg-flex mt-5">
+          <section className="d-none d-lg-flex mt-5 letsTalkBtnSec">
             <LetsTalkBtnContainer />
           </section>
         </section>
