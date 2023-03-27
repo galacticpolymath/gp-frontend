daff36c374963fdc25f23222ff4f1f8c5c9313d9
Gabe <gtorion97@gmail.com>
2023-03-26 21:45:44 -0500

    refactoring LiNavDot.js file

--- components/LessonSection/LessonSecsNavDots.js
+++ components/LessonSection/LessonSecsNavDots.js
@@ -16,6 +16,7 @@
 import { useEffect, useState } from "react";
 import LiNavDot from "./NavDots/LiNavDot";
 import { v4 } from "uuid";
+import SectionTitlesUl from "./NavDots/SectionTitlesUl";
 
 const LessonsSecsNavDots = ({ _sectionDots }) => {
     const [sectionDots, setSectionDots] = _sectionDots;
@@ -51,7 +52,6 @@ const LessonsSecsNavDots = ({ _sectionDots }) => {
                 willShowTitle: false,
             }
         }));
-        
         !willShowTitles && setWillShowTitles(true);
     }
 
@@ -76,37 +76,7 @@ const LessonsSecsNavDots = ({ _sectionDots }) => {
 
     return (
         <div className="position-fixed lessonSecsNavDotsListContainer d-flex">
-            <ul className='ps-0 mb-0  d-lg-flex flex-column position-relative justify-content-center align-items-center h-100' style={{ transform: 'translate3d(0px, 0px, 0px)', 'transition-duration': '3500ms', transition: 'all .15s ease-in' }}>
-                {sectionDots.map(({ isInView, SectionTitle, willShowTitle, sectionId }, index) => {
-                    
-                    return (
-                        <div key={index}>
-                            <li
-                                className='d-none d-lg-inline-flex justify-content-end'
-                                role='button'
-                                style={{ borderColor: !isInView ? 'rgb(190, 190, 190)' : '#363636 1px solid !important', opacity: willShowTitles ? 1 : 0, transition: "all .15s ease-in", height: "30px", 'width': '200px', transitionProperty: 'border-color, opacity' }}
-                            >
-                                <span style={{ transition: "all .15s ease-in", backgroundColor: isInView ? '#d5e6f3' : 'white', border: '#363636 1px solid', transitionProperty: 'border, background-color, opacity', fontWeight: 400, padding: 6 }} className='text-black  text-nowrap shadow rounded d-lg-inline-flex justify-content-center align-items-center d-none'>
-                                    {SectionTitle}
-                                </span>
-                            </li>
-                            <li
-                                className='d-lg-none d-inline-flex justify-content-end position-relative'
-                                role='button'
-                                style={{ border: 'none', opacity:  willShowTitle ? 1 : 0, transition: "all .15s ease-in", height: "30px", 'width': '200px', transitionProperty: 'border-color, display', zIndex: willShowTitle ? 100 : -1 }}
-                            >
-                                <span
-                                    style={{ transition: "all .15s ease-in", backgroundColor: isInView ? '#d5e6f3' : 'white', border: '#363636 1px solid', transitionProperty: 'background-color, opacity, border', fontWeight: 400, padding: 6 }}
-                                    className='text-black text-nowrap shadow rounded d-inline-flex d-lg-none justify-content-center align-items-center'
-                                    onClick={_ => goToSection(sectionId, true)}
-                                >
-                                    {SectionTitle}
-                                </span>
-                            </li>
-                        </div>
-                    )
-                })}
-            </ul>
+            <SectionTitlesUl sectionDots={sectionDots} willShowTitles={willShowTitles} goToSection={goToSection} />
             <ul onMouseEnter={handleMouseEnterIconList} onMouseLeave={handleMouseLeaveIconList} className='ps-0 d-none d-lg-flex flex-column position-relative justify-content-center align-items-center h-100' style={{ transform: 'translate3d(0px, 0px, 0px)', 'transition-duration': '3500ms', transition: 'all .15s ease-in' }}>
                 {sectionDots.map(({ isInView, sectionId }, index) => (
                     <LiNavDot
