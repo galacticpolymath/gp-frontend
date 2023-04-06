28528c0a8cc67a350fcc8acf38e6ceccbed6cd7b
Gabe <gtorion97@gmail.com>
2023-04-06 13:33:04 -0500

    testing  build result for github, will push

--- pages/lessons/[loc]/[id].js
+++ pages/lessons/[loc]/[id].js
@@ -91,6 +91,8 @@ const LessonDetails = ({ lesson, availLocs }) => {
     }
   }, [inView])
 
+  // testing push to github
+
   const shareWidgetFixedProps = isOnProduction ? { isOnSide: true, pinterestMedia: lesson.CoverImage.url } : { isOnSide: true, pinterestMedia: lesson.CoverImage.url, developmentUrl: `${lesson.URL}/` }
 
   return (
