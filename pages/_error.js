9a7c99980f89c0912ea08ae93ba1a373ad9e0c26
Gabe <gtorion97@gmail.com>
2023-04-16 16:45:34 -0500

    changed getServerSideProps

--- pages/_error.js
+++ pages/_error.js
@@ -29,7 +29,7 @@ export const getServerSideProps = async (context) => {
     return {
       redirect: {
         destination: `/lessons/${targetLesson.DefaultLocale}/${targetLesson.id}`,
-        permanent: false,
+        permanent: true,
       },
     }
   }
