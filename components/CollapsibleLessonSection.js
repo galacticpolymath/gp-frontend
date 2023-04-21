ba39e36cadf9cc85cf5ad07dbf5f752c92e1fa6e
Gabe <gtorion97@gmail.com>
2023-04-21 12:28:21 -0500

    fixing typo in jobViz and lessons bug

--- components/CollapsibleLessonSection.js
+++ components/CollapsibleLessonSection.js
@@ -20,7 +20,8 @@ const CollapsibleLessonSection = ({
 }) => {
   const ref = useRef();
   const { h2Id } = useLessonElementInView(_sectionDots, SectionTitle, ref, isAvailLocsMoreThan1);
-
+  const _h2Id = SectionTitle.toLowerCase().replace(/[0-9.]/g, "").trim().replace(/ /g, "-");
+  
   return (
     <Accordion
       initiallyExpanded={initiallyExpanded}
@@ -39,6 +40,7 @@ const CollapsibleLessonSection = ({
           <i className="fs-3 bi-chevron-down"></i>
           <i className="fs-3 bi-chevron-up"></i>
           <div id={h2Id} style={{ height: 30, width: 30, transform: 'translateY(-150px)' }} className='position-absolute' />
+          <div id={_h2Id} style={{ height: 30, width: 30, transform: 'translateY(-150px)' }} className='position-absolute' />
         </div>
       )}
     >
