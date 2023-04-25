35f6d51b96c4933eb6e43917b4f20e3487bfb724
Matt Wilkins <matt@galacticpolymath.com>
2023-04-25 18:39:08 -0500

    added vertical epaulette!

--- components/LessonSection/Overview.js
+++ components/LessonSection/Overview.js
@@ -16,6 +16,7 @@ const Overview = ({
   ForGrades,
   TargetSubject,
   SteamEpaulette,
+  SteamEpaulette_vert,
   Text,
   Tags,
   _sectionDots,
@@ -35,21 +36,21 @@ const Overview = ({
       <div ref={ref} className='container mb-4'>
         <div className="bg-light-gray px-4 py-2 mt-4 rounded-3 text-center">
           {LearningSummary && (
-          
-          <div className="g-col-12 bg-white p-3 rounded-3 mt-2 text-start  align-items-center">
-            <Image
-                        src="/imgs/gp_logo_gradient_transBG.png"
-                        alt="Galactic_PolyMath_First_Sec_Mobile_Info"
-                        style={{ objectFit: "contain" }}
-                        className="d-inline-flex me-2"
-                        height={30}
-                        width={30}
-                    />
-            <h5 className="d-inline-flex">The Gist:</h5>
-            <div>
-            {LearningSummary}
+
+            <div className="g-col-12 bg-white p-3 rounded-3 mt-2 text-start  align-items-center">
+              <Image
+                src="/imgs/gp_logo_gradient_transBG.png"
+                alt="Galactic_PolyMath_First_Sec_Mobile_Info"
+                style={{ objectFit: "contain" }}
+                className="d-inline-flex me-2 mb-2"
+                height={30}
+                width={30}
+              />
+              <h5 className="d-inline-flex">The Gist:</h5>
+              <div>
+                {LearningSummary}
+              </div>
             </div>
-          </div>
           )}
           <div className="grid mx-auto gap-3 py-3 justify-content-center justify-content-sm-start">
             <div className='d-none d-sm-grid g-col g-col-6 g-col-sm-4 bg-white p-3 rounded-3'>
@@ -89,25 +90,55 @@ const Overview = ({
               </div>
             </div>
           </div>
-          <Link passHref href="#learning_standards">
-            Subject breakdown by standard alignments:
-            {SteamEpaulette && SteamEpaulette.url && (
-              <div  className='position-relative'>
-                <Image
-                  src={SteamEpaulette.url}
-                  alt="Subject breakdown by standard alignments"
-                  priority
-                  height={160}
-                  width={2200}
-                  style={{
-                    objectFit: 'contain',
-                    height: "auto",
-                    width: "100%"
-                  }}
-                />
+
+          {SteamEpaulette && SteamEpaulette.url && (
+            <Link passHref href="#learning_standards">
+              <div className='position-relative'>
+                <div className="d-none d-sm-grid">
+                  Subject breakdown by standard alignments:
+                  <Image
+                    src={SteamEpaulette.url}
+                    alt="Subject breakdown by standard alignments"
+                    priority
+                    height={160}
+                    width={2200}
+                    style={{
+                      objectFit: 'contain',
+                      height: "auto",
+                      width: "100%"
+                    }}
+
+                  />
+                </div>
+
+
+
+                <div className="d-sm-flex d-sm-none  row justify-content-start pb-2">
+                  <Image
+                    src={SteamEpaulette_vert.url}
+                    alt="Subject breakdown by standard alignments"
+                    priority
+                    height={1320}
+                    width={320}
+                    style={{
+                      objectFit: 'contain',
+                      height: "80vw",
+                      width: "auto"
+                    }}
+                    className='col p-0 d-flex align-self-end'
+                  />
+                  <div className="col text-start align-content-center mt-3">
+                    <i class="bi bi-arrow-90deg-left fs-2 mb-0 d-flex "></i>
+                    <div className="rounded p-1 mt-0 d-flex"
+                      style={{ border: '2px solid ' }}>
+                      <h5>Subject breakdown by standard alignments</h5>
+                    </div>
+                  </div>
+                </div>
               </div>
-            )}
-          </Link>
+            </Link>
+          )}
+
         </div>
         <RichText className='mt-4' content={Text} />
 
