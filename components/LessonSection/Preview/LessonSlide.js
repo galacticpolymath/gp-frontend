485124703e32122d56f354c6ccf87fbafbd0e402
Matt Wilkins <matt@galacticpolymath.com>
2023-04-26 17:45:18 -0500

    futzed with media browser to make it better use the space on all devices

--- components/LessonSection/Preview/LessonSlide.js
+++ components/LessonSection/Preview/LessonSlide.js
@@ -13,18 +13,19 @@ const LessonSlide = ({
   mainLink,
 }) => {
   return (
-    <div className='autoCarouselItem onLessonsPg mb-1 rounded px-5 py-1 px-lg-1 py-lg-1 justify-content-center align-items-center'>
-      <div className='bg-white pb-3 pe-3 pe-sm-0 ps-3 ps-sm-0 pt-2 pt-0 p-sm-4 p-lg-3 rounded' style={{ maxWidth: 785 }}>
-        <div className='px-sm-2 px-md-0  justify-content-center d-flex align-items-center iframeContainer'>
+    <div className='autoCarouselItem onLessonsPg mb-1 rounded p-1 justify-content-center align-items-center'>
+      <div className='bg-white px-1 pt-2 pb-0 rounded ' >
+        <div className='px-1 justify-content-center d-flex' 
+        style={{position: "relative",border: "1px solid lightgray", height:"60vh", maxHeight: "60vw",objectFit:"contain"}}>
           {getMediaComponent({ type, mainLink })}
         </div>
-        <div className={`${styles.SlideBody} mt-3 mt-sm-4 lessonSlideBody`}>
+        <div className={`${styles.SlideBody} mt-2 text-wrap lessonSlideBody px-1 px-md-3`}>
           {forPart && (
             <div className='badge badge-pill bg-primary-light mb-1' style={{ color: 'gray' }}>for Part {forPart}</div>
           )}
-          <h5 style={{ whiteSpace: 'pre-line' }} className='text-left pb-2 pb-sm-0 p-sm-0 p-sm-4 mb-0 mb-sm-1 text-sm-start lessonTitle'>{title}</h5>
-          <p style={{ whiteSpace: 'pre-line' }} className="text-left pb-3 pb-sm-0 p-sm-0 p-sm-4 mb-0 mb-sm-1 text-sm-start lessonRelevanceTxt">{lessonRelevance}</p>
-          <span style={{ whiteSpace: 'pre-line' }} className="text-left p-sm-4 text-sm-start d-block d-sm-inline w-100">by{' '}
+          <h6  className='m-0 mb-1 fw-bolder fst-italic'>{title}</h6>
+          <p  className="lessonRelevanceTxt mb-2">{lessonRelevance}</p>
+          <span className=" d-block d-sm-inline w-100">by{' '}
             <a
               href={byLink}
               target="_blank"
