4895724c666888ea125211b2fd3f8a95dcb1aaa8
Toriong <gtorion97@gmail.com>
2023-05-06 14:42:14 -0500

    fixing responsiveness on Hire us page

--- components/HireUsComps/sections/IntroSecHireUs.js
+++ components/HireUsComps/sections/IntroSecHireUs.js
@@ -10,17 +10,17 @@ const IntroSecHireUs = () => {
       <section className="w-100 justify-content-center align-items-center d-none d-md-flex">
         <section className="d-none d-md-flex flex-column w-100 noMargin col-12 introSecAndLetTalksSec">
           <section className="d-flex flex-column w-100 justify-content-center align-items-center">
-            <section className="w-100 ps-5">
+            <section className="w-100 ps-md-5">
               <h1 className={`${styles.shadow} headingHireUs`}>Easier, Classroom-Ready Outreach</h1>
             </section>
-            <section className="w-100 ps-5 position-relative d-flex mt-lg-4 mt-lg-0 mb-4 mb-lg-0">
+            <section className="w-100 ps-md-5 position-relative d-flex mt-lg-4 mt-lg-0 mb-4 mb-lg-0">
               <span className={`${styles.shadow} subTxtHeadingDesktop spanSubheader noMargin noPadding w-75`}>
                 We translate your work so that non-experts can teach mind-opening lessons.
               </span>
             </section>
           </section>
-          <section className="d-flex ps-md-5 letTalksMainBtnSec">
-            <section className="introMainBtnInnerSec mt-3">
+          <section className="d-flex letTalksMainBtnSec">
+            <section className="introMainBtnInnerSec ps-md-5 mt-3">
               <LetsTalkBtnContainer />
             </section>
           </section>
@@ -28,7 +28,7 @@ const IntroSecHireUs = () => {
       </section>
       <section className="d-flex d-md-none w-100 noMargin col-12 introSecAndLetTalksSec">
         <section className="w-100 d-flex justify-content-start align-items-stretch  flex-column justify-content-md-center align-items-md-center pt-4">
-          <section className="headerSection">
+          <section className="headerSection ps-1">
             <h1 className={`${styles.shadow} headingHireUs noMargin`}>Easier,</h1>
             <h1 className={`${styles.shadow} headingHireUs noMargin`}>Classroom-ready</h1>
             <h1 className={`${styles.shadow} headingHireUs noMargin`}>Outreach</h1>
@@ -38,7 +38,7 @@ const IntroSecHireUs = () => {
               We translate your work so that non-experts can teach mind-opening lessons.
             </span>
           </section>
-          <section className="ps-0 ps-sm-1 pt-4 pt-md-5 letTalksMainBtnSec">
+          <section className="ps-1 ps-sm-1 pt-4 pt-md-5 letTalksMainBtnSec">
             <LetsTalkBtnContainer isMainBtn isNoUnderline />
           </section>
         </section>
