ef9ef6c8a273ad0058db5a7b96f7c943640e9ca4
Matt Wilkins <matt@galacticpolymath.com>
2023-05-10 16:48:13 -0500

    simplify should I reach out section

--- components/HireUsComps/sections/WhenShouldIReachOutSec.js
+++ components/HireUsComps/sections/WhenShouldIReachOutSec.js
@@ -21,26 +21,18 @@ import LetsTalkBtnContainer from "../buttons/LetsTalkBtnContainer"
 
 const WhenShouldIReachOutSec = () => {
     return (
-        <section className="mt-5 ps-sm-5 pe-sm-5 d-flex pb-2 pb-sm-5 pb-xl-0 flex-column">
-            <section className="ps-2 pe-2 ps-sm-0 pe-sm-0">
-                <h3 className="noMargin">When should I reach out?</h3>
-            </section>
-            <section className="d-flex flex-column flex-md-row anyTimeSec">
-                <section className="mt-3">
-                    <span className="text-dark d-block w-100 w-sm-85 fw249 text-center text-sm-start ps-2 pe-2 ps-sm-0 pe-sm-0 pe-xl-5">
-                        Any time! Whether you have funds now, want to write us into a proposal, or just have a question, we are here to help!
-                    </span>
-                </section>
-                <section className="d-flex mt-4 d-sm-block justify-content-center justify-content-sm-start align-items-sm-stretch align-items-sm-center">
-                    <div className="d-flex justify-content-center align-items-center d-md-none d-sm-block btnContainerWhenShouldIReach">
-                        <LetsTalkBtnContainer isDarker />
-                    </div>
-                    <div className="d-none d-md-block btnContainerWhenShouldIReach">
-                        <LetsTalkBtnContainer isNoUnderline isDarker />
-                    </div>
-                </section>
-            </section>
-        </section>
+        <section className="row text-dark my-5 py-3">
+            <div className="col-1 col-3-md"/>
+            <div className="col-6">
+                <h2 className="text-left ">When should I reach out?</h2>
+                <span className="fs-5">
+                    Any time! Whether you have funds now, want to write us into a proposal, or just have a question, we are here to help!
+                </span>
+                <div className="d-flex justify-content-left ps-3 mt-4">
+                    <LetsTalkBtnContainer isDarker />
+                </div>
+            </div>
+        </section >
     )
 }
 
