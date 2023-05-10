4dbe245991b699ec1d33624fb09aa4c6539ab6d3
Matt Wilkins <matt@galacticpolymath.com>
2023-05-10 17:21:49 -0500

    tweaking howmuch does it cost

--- components/HireUsComps/sections/HowMuchDoesItCostSec.js
+++ components/HireUsComps/sections/HowMuchDoesItCostSec.js
@@ -25,13 +25,12 @@ const HowMuchDoesItCostSec = ({ setTiersInfoForModalArr }) => (
         <section className="pt-5 ps-sm-5 pe-sm-5">
             <h3 className="noMargin">How much does it cost?</h3>
         </section>
-        {/* mt-4 mb-5 */}
-        <section className="w-100 d-flex justify-content-start align-items-center flex-row flex-md-column ps-1 pe-1 ps-sm-5 pe-sm-5 mt-sm-4 mb-3">
-            <span className="pt-3 pt-sm-0 fw200 fst-italic text-dark text-center text-sm-start ps-1 ps-sm-0 pe-1 pe-sm-0">
-                We offer three standard packages, which we are happy to customize to meet your specific needs.
-                <span className="d-md-none ms-1">Here are three standard options to use as a starting point.</span>
-            </span>
-            <span className="d-none d-md-inline fw200 fst-italic text-dark mt-2">Here are three standard options to use as a starting point<span className='d-inline d-xl-none'>.</span><span className='d-none d-xl-inline'>:</span></span>
+        <section className="w-100 text-dark d-flex justify-content-start align-items-center flex-row flex-md-column ps-1 pe-1 ps-sm-5 pe-sm-5 mt-sm-4 mb-3">
+            <div className="pt-3 pt-sm-0 fw200 fst-italic text-center text-sm-start ps-1 ps-sm-0 pe-1 pe-sm-0">
+                We offer three standard packages, which we’re happy to customize to meet your specific needs.
+            </div>
+            <span className="d-md-none ms-2">Here are three standard options to use as a starting point.</span>
+            <span className="d-none d-md-inline fst-italic mt-2">Here are three standard options to use as a starting point.</span>
         </section>
         <section className="d-flex d-md-none d-xl-flex flex-column flex-xl-row ps-md-5 pe-md-5">
             {tiers.map((tier, index) => {
