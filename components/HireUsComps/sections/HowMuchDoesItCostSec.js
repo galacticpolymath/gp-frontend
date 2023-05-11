dc384a6006653f997d61f9b097ccf85f0268063f
Matt Wilkins <matt@galacticpolymath.com>
2023-05-10 20:10:48 -0500

    fixed how much does it cost and when should I reach out responsiveness

--- components/HireUsComps/sections/HowMuchDoesItCostSec.js
+++ components/HireUsComps/sections/HowMuchDoesItCostSec.js
@@ -21,17 +21,14 @@ import Tier from './Tier';
 
 
 const HowMuchDoesItCostSec = ({ setTiersInfoForModalArr }) => (
-    <section className="howMuchDoesItCostSec pb-5 mb-3">
-        <section className="pt-5 ps-sm-5 pe-sm-5">
-            <h3 className="noMargin">How much does it cost?</h3>
-        </section>
-        <section className="w-100 text-dark d-flex justify-content-start align-items-center flex-row flex-md-column ps-1 pe-1 ps-sm-5 pe-sm-5 mt-sm-4 mb-3">
-            <div className="pt-3 pt-sm-0 fw200 fst-italic text-center text-sm-start ps-1 ps-sm-0 pe-1 pe-sm-0">
-                We offer three standard packages, which we’re happy to customize to meet your specific needs.
-            </div>
-            <span className="d-md-none ms-2">Here are three standard options to use as a starting point.</span>
-            <span className="d-none d-md-inline fst-italic mt-2">Here are three standard options to use as a starting point.</span>
-        </section>
+    <section className="howMuchDoesItCostSec pb-5 mb-3 mt-3">
+        <div className='ms-0 ms-md-5 ps-0 ps-md-5 pt-5 text-start'>
+            <h3 >How much does it cost?</h3>
+            <section className="w-100 text-dark fst-italic d-grid justify-content-start ">
+                <div >Here are three customizable options to use as a starting point.</div>
+            </section>
+        </div>
+        {/* mobile */}
         <section className="d-flex d-md-none d-xl-flex flex-column flex-xl-row ps-md-5 pe-md-5">
             {tiers.map((tier, index) => {
                 return (
@@ -45,6 +42,7 @@ const HowMuchDoesItCostSec = ({ setTiersInfoForModalArr }) => (
                 )
             })}
         </section>
+        {/* desktop */}
         <section className="d-none d-md-flex d-xl-none flex-column justify-content-center align-items-center ">
             {tiers.map((tier, index) => {
                 return (
--- components/HireUsComps/sections/WhenShouldIReachOutSec.js
+++ components/HireUsComps/sections/WhenShouldIReachOutSec.js
@@ -22,8 +22,8 @@ import LetsTalkBtnContainer from "../buttons/LetsTalkBtnContainer"
 const WhenShouldIReachOutSec = () => {
     return (
         <section className="row text-dark my-5 py-3">
-            <div className="col-1 col-3-md"/>
-            <div className="col-6">
+            <div className="col-1 col-md-2"/>
+            <div className="col-12 col-md-6">
                 <h2 className="text-left ">When should I reach out?</h2>
                 <span className="fs-5">
                     Any time! Whether you have funds now, want to write us into a proposal, or just have a question, we are here to help!
