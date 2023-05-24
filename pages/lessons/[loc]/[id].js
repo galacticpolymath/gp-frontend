fca332db91b5d8e21f21724f00c74f761da304ad
Toriong <gtorion97@gmail.com>
2023-05-24 08:27:11 -0500

    pushing to remote, checking build

--- pages/lessons/[loc]/[id].js
+++ pages/lessons/[loc]/[id].js
@@ -27,9 +27,9 @@ const NAV_CLASSNAMES = ['sectionNavDotLi', 'sectionNavDot', 'sectionTitleParent'
 
 const getLatestSubRelease = (sections) => {
   const versionSection = sections.versions;
-  if (!versionSection) {
-    return null;
-  }
+
+  if (!versionSection) return null;
+  
 
   const lastRelease = versionSection.Data[versionSection?.Data?.length - 1].sub_releases;
   const lastSubRelease = lastRelease[lastRelease?.length - 1];
