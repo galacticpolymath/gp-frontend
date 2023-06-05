10c5b95a16b3480fdab25df8f28e41532524a08c
Toriong <gtorion97@gmail.com>
2023-06-05 14:30:49 -0500

    connecting to mongodb

--- pages/api/insert-lesson.js
+++ pages/api/insert-lesson.js
@@ -1,7 +1,19 @@
-
+import { insertLesson } from '../../backend/services/lessonsServices'
 
 export default function handler(request, response) {
     console.log('Hey there, request has been received.')
+    console.log('request?.body: ', request?.body.lesson)
+    const bodyParsed = JSON.parse(request.body)
+    console.log('bodyParsed: ', bodyParsed)
 
-    return response.status(200).json({ msg: "'/insert-lesson' route is live! Listening for tests to insert into the database." })
+    if((request.method === 'POST') && request?.body){
+        const { wasSuccessful, errMsg } = insertLesson(request.body.lesson)
+
+        if(wasSuccessful){
+            return response.status(200).json({ msg: `'${request.body.lesson.title}' was inserted into the database.` })
+        }
+    }
+
+    
+    response.status(404).json({ msg: "Request info was incorrect. Only accept POST request." })
 }
--- unit-testing/api/isTestWorking.test.js
+++ unit-testing/api/isTestWorking.test.js
@@ -4,11 +4,11 @@ const { insertLesson } = require('../../api-call-fns/lessons/insertLesson.js')
 
 describe("Testing connection of route '/insert-lesson'", () => {
     it('Should make a successful API call. Should receive a 200 response.', async () => {
-        const response = await insertLesson({ data: 'This is a test' })
+        const response = await insertLesson({ lesson: { title: 'This is a test'} })
         const isObj = typeof response === 'object'
 
         expect(isObj).toBe(true)
         expect(response.status).toBe(200)
-        expect(response.data.msg).toBe("'/insert-lesson' route is live! Listening for tests to insert into the database.")
+        expect(response.data.msg).toBe("'This is a test' was inserted into the database.")
     });
 });
