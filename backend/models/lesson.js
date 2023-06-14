ff0cee7d5be03ddcf6f07edaf900e6f7f84cbc68
Toriong <gtorion97@gmail.com>
2023-06-14 08:52:00 -0500

    updated lesson schema

--- backend/models/lesson.js
+++ backend/models/lesson.js
@@ -1,23 +1,64 @@
 import Mongoose from 'mongoose';
-const { Schema, models } = Mongoose;
+const { Schema, models, model } = Mongoose;
 
 let Lessons = models.lessons;
 
 if (!Lessons) {
-    const requiredNumberVal = {
-        type: Number,
-        required: true,
-    };
-    const requiredStringVal = {
-        type: String,
-        required: true,
-    };
-    const lessonSchema = new Schema({
-        _id: requiredNumberVal,
-        Title: requiredStringVal
+    const LessonSchema = new Schema({
+        id: { type: Number, required: true },
+        UniqueID: String,
+        ShortTitle: String,
+        PublicationStatus: String,
+        Language: String,
+        Country: String,
+        DefaultLanguage: String,
+        DefaultCountry: String,
+        LastUpdated: { type: Date, default: Date.now },
+        FirstPublicationDate: { type: Date, default: Date.now },
+        MediumTitle: String,
+        lang: String,
+        lng: String,
+        locale: { type: String, required: true },
+        TemplateVer: String,
+        galacticPubsVer: String,
+        isTestRepo: Boolean,
+        defaultLocale: String,
+        URL: String,
+        GPCatalogURL: String,
+        GitHubURL: String,
+        GdriveHome: String,
+        GdriveDirName: String,
+        GdriveTeachMatPath: String,
+        GdriveTeachMatID: String,
+        GdrivePublicID: Number,
+        GdriveDirID: String,
+        GdriveMetaID: String,
+        GdriveTeachItID: String,
+        GdriveStandardsID: String,
+        GdrivePublishedID: String,
+        GdriveDirURL: String,
+        RebuildAllMaterials: Boolean,
+        ReleaseDate: Date,
+        Title: { type: String, required: true },
+        Subtitle: String,
+        SponsorName: [String],
+        SponsoredBy: String,
+        SponsorLogo: [String],
+        LessonBanner: String,
+        TargetSubject: String,
+        LessonEnvir: String,
+        ForGrades: String,
+        GradesOrYears: String,
+        SponsorImage: {
+            url: [String]
+        },
+        CoverImage: {
+            url: String
+        },
+        Section: Schema.Types.Mixed,
     });
-
-    Lessons = Mongoose.model('lessons', lessonSchema);
+    
+    Lessons = model('Lessons', LessonSchema);
 }
 
 export { Lessons } 
