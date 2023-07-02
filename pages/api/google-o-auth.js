bcb45fc1a3d9376912324b358fe11d86d97cc175
Toriong <gtorion97@gmail.com>
2023-07-02 16:25:25 -0500

    refresh token is working, will make a path to send a new code to the client

--- pages/api/google-o-auth.js
+++ pages/api/google-o-auth.js
@@ -1,18 +1,23 @@
 import { google } from 'googleapis';
 
 
-export default async function handler(_, response) {    
+export default async function handler(request, response) {
+    // determine where the request originated from in order to get the appropriate redirect uri
     const { AUTH_CLIENT_ID, AUTH_CLIENT_SECRET, AUTH_REDIRECT_URI } = process.env;
     const oauth2Client = new google.auth.OAuth2(
         AUTH_CLIENT_ID,
         AUTH_CLIENT_SECRET,
         AUTH_REDIRECT_URI
     );
-    const scopes = [];
+    const SCOPES = [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email',
+    ];
     const url = oauth2Client.generateAuthUrl({
         access_type: 'offline',
-        scope: scopes,
+        scope: SCOPES,
     });
 
+    // The url will be sent to the client side in a json body.
     return response.redirect(url)
 }
