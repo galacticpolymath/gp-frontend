cdff109edc017c80b817c6b0bcc58098a9043d5a
Toriong <gtorion97@gmail.com>
2023-07-05 10:39:36 -0500

    tokens working locally

--- apiServices/googleOAuth/googleOAuth.js
+++ apiServices/googleOAuth/googleOAuth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { google } from 'googleapis';
 
 
@@ -20,4 +21,46 @@ function generateRedirectUrl(requestOrigin) {
     });
 }
 
-export { generateRedirectUrl }
+function generateReqBodyForIdToken(refreshToken, idToken) {
+    const { AUTH_CLIENT_ID, AUTH_CLIENT_SECRET, AUTH_REDIRECT_URI } = process.env;
+
+    if (refreshToken) {
+        return {
+            client_id: AUTH_CLIENT_ID,
+            client_secret: AUTH_CLIENT_SECRET,
+            refresh_token: refreshToken,
+            grant_type: 'refresh_token'
+        }
+    }
+
+    return {
+        client_id: AUTH_CLIENT_ID,
+        client_secret: AUTH_CLIENT_SECRET,
+        code: idToken,
+        redirect_uri: AUTH_REDIRECT_URI,
+        grant_type: 'authorization_code'
+    }
+
+}
+
+async function getIdToken(idToken, refreshToken) {
+    try {
+        const googleApisUrl = 'https://oauth2.googleapis.com/token';
+        const postReqBody = idToken ? generateReqBodyForIdToken(null, idToken) : generateReqBodyForIdToken(refreshToken, null);
+        const response = await axios.post(googleApisUrl, postReqBody);
+
+        if (response.status === 200) {
+            const _data = idToken ? { refreshToken: response.data.refresh_token, idToken: response.data.id_token }: { idToken: response.data.id_token }
+            
+            return { status: 200, data: _data }
+        }
+
+        throw new Error('An error has occurred in getting the id token from google apis.')
+    } catch (error) {
+        console.error("An error has occurred in getting the id token from google apis: ", error);
+
+        return { status: 500, msg: `Error message: ${error}` }
+    }
+}
+
+export { generateRedirectUrl, getIdToken }
--- pages/api/get-id-token.js
+++ pages/api/get-id-token.js
@@ -1,6 +1,29 @@
 import { google } from 'googleapis';
+import { getIdToken } from '../../apiServices/googleOAuth/googleOAuth';
 
 
 export default async function handler(request, response) {
-    // this function will handle the request in order to get the id-token from google apis
+    if(request.method !== 'POST'){
+        return response.status(405).json({ msg: 'Method not allowed.' })
+    }
+
+    console.log('request?.body: ', request?.body)
+
+    if(!request?.body?.code && !request?.body?.refreshToken){
+        console.error('Either the code or the refresh token is missing from the request body.')
+
+        return response.status(400).json({ msg: 'Either the code or the refresh token is missing from the request body.' })
+    }
+
+    if(request?.body?.code){
+        const { data, status, msg } = await getIdToken(request.body.code);
+
+        return (status !== 200) ? response.status(status).json({ msg: msg }) : response.status(status).json({ data: data }) 
+    }
+
+    if(request.body.refreshToken){
+        const { data, status, msg } = await getIdToken(null, request.body.refreshToken);
+
+        return (status !== 200) ? response.status(status).json({ msg: msg }) : response.status(status).json({ data: data }) 
+    }
 }
