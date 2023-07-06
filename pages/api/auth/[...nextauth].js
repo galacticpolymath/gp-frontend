cae2bf16f3019b0d80a0c4964e40b591153efd2e
Toriong <gtorion97@gmail.com>
2023-07-06 08:14:23 -0500

    getting auth token

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,18 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.AUTH_CLIENT_ID,
-      clientSecret: process.env.AUTH_CLIENT_SECRET,
-      
-    }),
-  ],
+    providers: [
+        GoogleProvider({
+            clientId: process.env.AUTH_CLIENT_ID,
+            clientSecret: process.env.AUTH_CLIENT_SECRET,
+
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user, account, profile, isNewUser }) {
+            return token
+        }
+    }
 }
 
 export default NextAuth(authOptions)
