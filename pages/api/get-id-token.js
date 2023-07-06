ec302fac6771c3308652064fda77503407f5bf9f
Toriong <gtorion97@gmail.com>
2023-07-06 08:17:50 -0500

    getting access token, wip

--- pages/api/get-id-token.js
+++ pages/api/get-id-token.js
@@ -1,29 +1,24 @@
 import { google } from 'googleapis';
-import { getIdToken } from '../../apiServices/googleOAuth/googleOAuth';
+import { getServerSession } from 'next-auth';
 
+async function getAccessToken(request) {
+    const session = await getServerSession({ request });
+
+    return session?.accessToken;
+};
 
 export default async function handler(request, response) {
-    if(request.method !== 'POST'){
-        return response.status(405).json({ msg: 'Method not allowed.' })
-    }
+    try {
+        const accessToken = await getAccessToken(request);
 
-    console.log('request?.body: ', request?.body)
+        return response.status(200).json({
+            accessToken: accessToken
+        });
+    } catch (error) {
+        console.error('Something went wrong getting the access token: ', error);
 
-    if(!request?.body?.code && !request?.body?.refreshToken){
-        console.error('Either the code or the refresh token is missing from the request body.')
-
-        return response.status(400).json({ msg: 'Either the code or the refresh token is missing from the request body.' })
-    }
-
-    if(request?.body?.code){
-        const { data, status, msg } = await getIdToken(request.body.code);
-
-        return (status !== 200) ? response.status(status).json({ msg: msg }) : response.status(status).json({ data: data }) 
-    }
-
-    if(request.body.refreshToken){
-        const { data, status, msg } = await getIdToken(null, request.body.refreshToken);
-
-        return (status !== 200) ? response.status(status).json({ msg: msg }) : response.status(status).json({ data: data }) 
+        return response.status(500).json({
+            error: 'Internal server error. Failed to generate the access token.'
+        });
     }
 }
