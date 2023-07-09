45463bf6813757d7ae8ea93d9eaf495ab5f8d33e
Toriong <gtorion97@gmail.com>
2023-07-09 07:45:52 -0500

    inserting lesson into the db with authorization of request

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,39 +9,6 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.AUTH_CLIENT_ID,
             clientSecret: process.env.AUTH_CLIENT_SECRET,
-        }),
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. 'Sign in with...')
-            name: 'Credentials',
-            // The credentials is used to generate a suitable form on the sign in page.
-            // You can specify whatever fields you are expecting to be submitted.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                username: { label: "Email", type: "text", placeholder: "Enter email" },
-                password: { label: "Password", type: "password", placeholder: "Enter password" }
-            },
-            async authorize(credentials, req) {
-                // You need to provide your own logic here that takes the credentials
-                // submitted and returns either a object representing a user or value
-                // that is false/null if the credentials are invalid.
-                // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-                // You can also use the `req` object to obtain additional parameters
-                // (i.e., the request IP address)
-                const res = await fetch("/your/endpoint", {
-                    method: 'POST',
-                    body: JSON.stringify(credentials),
-                    headers: { "Content-Type": "application/json" }
-                })
-                const user = await res.json()
-
-                // If no error and we have user data, return it
-                if (res.ok && user) {
-                    return user
-                }
-                // Return null if user data could not be retrieved
-                return null
-            }
         })
     ],
     authorizationUrl:
@@ -80,9 +47,7 @@ export const authOptions = {
     callbacks: {
         async jwt({ token, user, account, profile, isNewUser }) {
             const isUserSignedIn = !!user;
-            console.log("user: ", user)
-            console.log('token: ', token)
-
+            
             if (isUserSignedIn) {
                 token.id = user.id.toString();
             }
@@ -99,7 +64,7 @@ export const authOptions = {
         async redirect({ url }) {
             const urlObj = new URL(url);
 
-            return Promise.resolve(`${urlObj.origin}/auth-results`);
+            return Promise.resolve(`${urlObj.origin}/auth-result`);
         }
     },
 
