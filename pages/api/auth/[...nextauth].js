c41f5ec6b419f8a489e48db1d781ca6a6e8b79ca
Toriong <gtorion97@gmail.com>
2023-07-09 19:57:33 -0500

    updating eslint to include es6 built in objects

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,89 +1,88 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import jwt from "jsonwebtoken"
-import User from "../../../backend/models/user"
-import { connectToMongodb } from "../../../backend/utils/connection"
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import jwt from 'jsonwebtoken';
+import Users from '../../../backend/models/user';
+import { connectToMongodb } from '../../../backend/utils/connection';
 
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.AUTH_CLIENT_ID,
-            clientSecret: process.env.AUTH_CLIENT_SECRET,
-        })
-    ],
-    authorizationUrl:
-        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
-    scope:
-        'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.readonly',
-    session: {
-        strategy: "jwt",
-    },
-    jwt: {
-        secret: process.env.NEXTAUTH_SECRET,
-        maxAge: 60 * 60 * 24 * 30, // 30 days
-        encode: async ({ secret, token }) => {
-            try {
-                await connectToMongodb();
-                const { email, name } = token
-                const user = await User.findOne({ _id: email }).lean();
-                let allowedRoles = ["user"];
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_CLIENT_ID,
+      clientSecret: process.env.AUTH_CLIENT_SECRET,
+    }),
+  ],
+  authorizationUrl:
+    'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+  scope:
+    'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.readonly',
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+    encode: async ({ secret, token }) => {
+      try {
+        await connectToMongodb();
+        const { email, name } = token;
+        const user = await Users.findOne({ _id: email }).lean();
+        let allowedRoles = ['user'];
 
-                if (user) {
-                    allowedRoles = [...allowedRoles, ...user.roles.map(({ role }) => role)]
-                    allowedRoles = [...new Set(allowedRoles)];
-                }
-
-                const jwtClaims = {
-                    "sub": email,
-                    "name": name,
-                    "email": email,
-                    "iat": Date.now() / 1000,
-                    "exp": Math.floor(Date.now() / 1000) + (24 * 60 * 60),
-                    "claims": {
-                        "allowedRoles": allowedRoles,
-                        "defaultRole": "user",
-                        "role": allowedRoles.find(role => role === 'dbAdmin') ?? "user",
-                        "userId": email,
-                    }
-                };
-                const encodedToken = jwt.sign(jwtClaims, secret, { algorithm: 'HS256' });
-
-                return encodedToken;
-            } catch (error) {
-                console.error('An error has occurred while encoding the jwt token: ', error)
-
-                throw new Error("Unable to generate JWT.");
-            }
-        },
-        decode: async ({ secret, token }) => {
-            const decodedToken = jwt.verify(token, secret, { algorithms: ['HS256'] });
-            return decodedToken;
-        },
-    },
-    callbacks: {
-        async jwt({ token, user, account, profile, isNewUser }) {
-            const isUserSignedIn = !!user;
-
-            if (isUserSignedIn) {
-                token.id = user.id.toString();
-            }
-
-            return Promise.resolve(token);
-        },
-        async session({ session, token }) {
-            const encodedToken = jwt.sign(token, process.env.NEXTAUTH_SECRET, { algorithm: 'HS256' });
-            session.id = token.id;
-            session.token = encodedToken;
-
-            return Promise.resolve(session);
-        },
-        async redirect({ url }) {
-            const urlObj = new URL(url);
-
-            return Promise.resolve(`${urlObj.origin}/auth-result`);
+        if (user) {
+          allowedRoles = [...allowedRoles, ...user.roles.map(({ role }) => role)];
+          allowedRoles = [...new Set(allowedRoles)];
         }
+
+        const jwtClaims = {
+          sub: email,
+          name,
+          email,
+          iat: Date.now() / 1000,
+          exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
+          claims: {
+            allowedRoles,
+            defaultRole: 'user',
+            role: allowedRoles.find((role) => role === 'dbAdmin') ?? 'user',
+            userId: email,
+          },
+        };
+        const encodedToken = jwt.sign(jwtClaims, secret, { algorithm: 'HS256' });
+
+        return encodedToken;
+      } catch (error) {
+        console.error('An error has occurred while encoding the jwt token: ', error);
+
+        throw new Error('Unable to generate JWT.');
+      }
     },
+    decode: async ({ secret, token }) => {
+      const decodedToken = jwt.verify(token, secret, { algorithms: ['HS256'] });
+      return decodedToken;
+    },
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      const isUserSignedIn = !!user;
 
-}
+      if (isUserSignedIn) {
+        token.id = user.id.toString();
+      }
 
-export default NextAuth(authOptions)
+      return Promise.resolve(token);
+    },
+    async session({ session, token }) {
+      const encodedToken = jwt.sign(token, process.env.NEXTAUTH_SECRET, { algorithm: 'HS256' });
+      session.id = token.id;
+      session.token = encodedToken;
+
+      return Promise.resolve(session);
+    },
+    async redirect({ url }) {
+      const urlObj = new URL(url);
+
+      return Promise.resolve(`${urlObj.origin}/auth-result`);
+    },
+  },
+};
+
+export default NextAuth(authOptions);
--- pages/auth-result.js
+++ pages/auth-result.js
@@ -1,75 +1,76 @@
-import { useEffect, useState } from "react";
-import Layout from "../components/Layout"
-import { useSession } from "next-auth/react"
-import Link from "next/link";
+import { useEffect, useState } from 'react';
+import Layout from '../components/Layout';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 const AuthResultPg = () => {
-    const session = useSession();
-    const [wasRendered, setWasRendered] = useState(false);
+  const session = useSession();
+  const [wasRendered, setWasRendered] = useState(false);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setWasRendered(true)
-        }, 500)
-    }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setWasRendered(true);
+    }, 500);
+  }, []);
 
-    if (!wasRendered) {
-        return (
-            <Layout>
-                <div style={{ minHeight: "100vh", paddingTop: "10px" }} className="container">
-                    <h1>Loading, please wait...</h1>
-                </div>
-            </Layout>
-        )
-    }
-
-    if (!session && wasRendered) {
-        return (
-            <Layout>
-                <div style={{ minHeight: "100vh", paddingTop: "10px" }} className="container">
-                    <h1>You must be sucessfully authenticated to view this page.</h1>
-                </div>
-            </Layout>
-        )
-    }
-
-    if (!session?.data?.token && wasRendered) {
-        return (
-            <Layout>
-                <div style={{ minHeight: "100vh", paddingTop: "10px" }} className="container">
-                    <h1>Failed to generate token. Please try again.</h1>
-                </div>
-            </Layout>
-        )
-    }
-
-    const { token, user } = session.data;
-    const isMatt = user.name === "Matt Wilkins"
-
+  if (!wasRendered) {
     return (
-        <Layout>
-            <div style={{ minHeight: "100vh", paddingTop: "10px" }} className="container">
-                <h1>Authenticated as: </h1>
-                <p>Name: {user.name}</p>
-                <p>Email: {user.email}</p>
-                <p style={{
-                    maxWidth: "475px",
-                    whiteSpace: 'initial',
-                    overflow: 'contain',
-                    wordWrap: 'break-word',
-                    paddingTop: "10px",
-                    paddingBottom: "20px",
-                    borderBottom: "1px solid grey"
-                }}
-                >
-                    JWT: {token}
-                </p>
-                <Link href="/">
-                    {isMatt ? "Go back to main page or check console." : "Go back to main page."}
-                </Link>
-            </div>
-        </Layout>
-    )
-}
+      <Layout>
+        <div style={{ minHeight: '100vh', paddingTop: '10px' }} className="container">
+          <h1>Loading, please wait...</h1>
+        </div>
+      </Layout>
+    );
+  }
 
-export default AuthResultPg
+  if (!session && wasRendered) {
+    return (
+      <Layout>
+        <div style={{ minHeight: '100vh', paddingTop: '10px' }} className="container">
+          <h1>You must be successfully authenticated to view this page.</h1>
+        </div>
+      </Layout>
+    );
+  }
+
+  if (!session?.data?.token && wasRendered) {
+    return (
+      <Layout>
+        <div style={{ minHeight: '100vh', paddingTop: '10px' }} className="container">
+          <h1>Failed to generate token. Please try again.</h1>
+        </div>
+      </Layout>
+    );
+  }
+
+  const { token, user } = session.data;
+  const isMatt = user.name === 'Matt Wilkins';
+
+  return (
+    <Layout>
+      <div style={{ minHeight: '100vh', paddingTop: '10px' }} className="container">
+        <h1>Authenticated as: </h1>
+        <p>Name: {user.name}</p>
+        <p>Email: {user.email}</p>
+        <p
+          style={{
+            maxWidth: '475px',
+            whiteSpace: 'initial',
+            overflow: 'contain',
+            wordWrap: 'break-word',
+            paddingTop: '10px',
+            paddingBottom: '20px',
+            borderBottom: '1px solid grey',
+          }}
+        >
+          JWT: {token}
+        </p>
+        <Link href="/">
+          {isMatt ? 'Go back to the main page or check the console.' : 'Go back to the main page.'}
+        </Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default AuthResultPg;
