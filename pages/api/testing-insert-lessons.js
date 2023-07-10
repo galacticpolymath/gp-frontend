a889d6e90eb3ebe9dbb750f7d182bbce17a35ac5
Toriong <gtorion97@gmail.com>
2023-07-09 19:47:57 -0500

    deleted unused google o-auth files

--- pages/api/testing-insert-lessons.js
+++
@@ -1,9 +0,0 @@
-import { getIsReqAuthorizedResult } from "../../backend/services/authServices";
-
-export default async function handler(request, response) {
-    const authorizationResult = await getIsReqAuthorizedResult(request);
-
-    console.log('authorizationResult.userCredentials.claims.allowedRoles: ', authorizationResult.userCredentials.claims.allowedRoles)
-
-    return response.status(200).json({ message: "test!" })
-}
