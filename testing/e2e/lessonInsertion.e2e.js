645da3015809528dce13da7a47ff8cb1b6b100d0
Toriong <gtorion97@gmail.com>
2023-07-18 12:48:34 -0500

    getting jwt manually to test insertion of lesson into the db

--- testing/e2e/lessonInsertion.e2e.js
+++
@@ -1,5 +0,0 @@
-// MAIN GOAL: this should insert a lesson into the database
-
-// GOAL: CREATE THE DUMMY lesson to insert into the databasse 
-
-// GOAL: get the jwt when the user is authenticated using NextAuth
