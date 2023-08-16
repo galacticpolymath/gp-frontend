529d119fa7f374d66dd445d6d944b26c55f8bd1c
Toriong <gtorion97@gmail.com>
2023-08-16 08:41:25 -0500

    testing middleware

---
+++ middleware.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { getIsReqAuthorizedResult } from "./backend/services/authServices";
+
+export default async function middleware(request) {
+    console.log('request: ', request)
+    console.log('nextUrl: ', nextUrl)
+    const { nextUrl, method, headers } = request;
+
+    if ((nextUrl.pathname == "/api/insert-lesson") && (method !== 'POST')) {
+        return new NextResponse(`Cannot access this endpoint with '${method}'`, { status: 400 })
+    }
+
+    if ((nextUrl.pathname == "/api/insert-lesson") && (method !== 'POST') && !headers.authorization) {
+        return new NextResponse("You cannot access this endpoint. 'authorization' field is not present in the headers", { status: 401 })
+    }
+
+    if ((nextUrl.pathname == "/api/insert-lesson") && (method === 'POST') && headers.authorization) {
+        const authorizationResult = await getIsReqAuthorizedResult(request, 'dbAdmin');
+
+        if (!authorizationResult?.isReqAuthorized) {
+            return new NextResponse("You are not authorized to access this service.", { status: 403 })
+        }
+
+        if (!Object.keys(request?.body)?.length) {
+            return new NextResponse("The request body is empty.", { status: 400 })
+        }
+
+        return NextResponse.next();
+    }
+
+    return new NextResponse("Invalid request parameters or body.", { status: 404 })
+}
