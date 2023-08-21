68f56ac05149e5fadff36363457ec008c4a67d90
Toriong <gtorion97@gmail.com>
2023-08-21 10:17:05 -0500

    will push to remote, testing build

--- pages/api/get-lessons.js
+++ pages/api/get-lessons.js
@@ -12,35 +12,47 @@ export default async function handler(request, response) {
   try {
     await connectToMongodb();
 
-    const { _id, numId, projections } = request.query;
+    let { _id, numId, projections } = request.query;
 
     if(_id && (typeof _id !== 'string')){
-      throw new Error("The '_id' query param must be a string.");
+      let errorObj = new Error("The '_id' query param must be a string.");
+      errorObj.code = 400;
+      throw errorObj;
     }
 
     if(numId && isNaN(parseInt(numId))){
-      throw new Error("The 'numId' must be a number.");
+      let errorObj = new Error("The 'numId' must be a number.");
+      errorObj.code = 400;
+      throw errorObj;
     }
 
-    console.log('projections: ', projections)
+    projections = typeof projections === 'string' ? JSON.parse(projections) : projections;
 
     if ((projections !== undefined) && !getIsObj(projections)) {
-      throw new Error("The 'projections' query param must be an object.");
+      let errorObj = new Error("The 'projections' query param must be an object.");
+      errorObj.code = 400;
+      throw errorObj;
     }
 
     const { wasSuccessful, data, msg } = (projections ? await retrieveLessonsResultObj(_id, numId, projections) : await retrieveLessonsResultObj(_id, numId)) ?? {}
 
     if (!wasSuccessful && msg) {
-      throw new Error(msg);
+      let errorObj = new Error(msg);
+      errorObj.code = 500;
+      throw errorObj;
     }
 
     if (!wasSuccessful) {
-      throw new Error("Failed to retrieve lessons from db.");
+      let errorObj = new Error("Failed to retrieve lessons from db.");
+      errorObj.code = 500;
+      throw errorObj;
     }
 
     return response.status(200).json({ lessons: data });
   } catch (error) {
+    console.log('Error obj within catch block: ', error)
+    const { code, message } = error;
 
-    return response.status(500).json({ msg: `Error message: ${error}` });
+    return response.status(code ?? 500).json({ msg: `Error message: ${message ?? 'An error has occurred on the server.'}` });
   }
 }
