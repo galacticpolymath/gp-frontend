abbb22c0cb51ce7aa554e1ff02d1845f6f92fc85
Toriong <gtorion97@gmail.com>
2023-08-20 21:26:43 -0500

    creating a route for getting lessons

--- middleware.js
+++ middleware.js
@@ -2,17 +2,19 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from 'jose'
 
 const getDoesUserHaveSpecifiedRole = (userRoles, targetRole = 'user') => !!userRoles.find(role => role === targetRole);
+const VALIDS_METHODS = ['POST']
+
 
 export async function middleware(request) {
     const { nextUrl, method, headers } = request;
     const authorizationStr = headers.get('authorization')
 
-    if ((nextUrl.pathname == "/api/insert-lesson") && (method !== 'POST')) {
-        return new NextResponse(`Cannot access this endpoint with '${method}'`, { status: 400 })
+    if(!nextUrl.pathname){
+        return new NextResponse("No pathName was provided.", { status: 400 })
     }
 
-    if ((nextUrl.pathname == "/api/insert-lesson") && (method !== 'POST') && !authorizationStr) {
-        return new NextResponse("You cannot access this endpoint. 'authorization' field is not present in the headers", { status: 401 })
+    if(!authorizationStr){
+        return new NextResponse("No authorization header was provided.", { status: 400 })
     }
 
     if ((nextUrl.pathname == "/api/insert-lesson") && (method === 'POST') && authorizationStr) {
@@ -24,7 +26,6 @@ export async function middleware(request) {
         }
 
         const isUserDbAdmin = getDoesUserHaveSpecifiedRole(payload.roles, 'dbAdmin');
-        console.log('isUserDbAdmin: ', isUserDbAdmin)
 
         if (!isUserDbAdmin) {
             return new NextResponse("You are not authorized to access this service.", { status: 403 })
@@ -33,7 +34,7 @@ export async function middleware(request) {
         return NextResponse.next();
     }
 
-    return new NextResponse("Invalid request parameters or body.", { status: 404 })
+    return new NextResponse("Invalid request parameters or body.", { status: 400 })
 }
 
 export const config = {
