44691128d5b5f054359c4e8a0cd9e09f9ef17cfa
Toriong <gtorion97@gmail.com>
2023-08-21 12:08:36 -0500

    added a CustomError class to get lesson path, testing build, will push to remote

--- pages/api/get-lessons.js
+++ pages/api/get-lessons.js
@@ -4,6 +4,13 @@ import { connectToMongodb } from '../../backend/utils/connection';
 
 const getIsObj = val => (val !== null) && (typeof val === 'object') && (Array.isArray(val) === false);
 
+class CustomError {
+  constructor(message, code) {
+    this.message = message;
+    this.code = code;
+  }
+}
+
 export default async function handler(request, response) {
   if (request.method !== 'GET') {
     return response.status(404).json({ msg: 'This route only accepts GET requests.' });
@@ -15,42 +22,32 @@ export default async function handler(request, response) {
     let { _id, numId, projections } = request.query;
 
     if(_id && (typeof _id !== 'string')){
-      let errorObj = new Error("The '_id' query param must be a string.");
-      errorObj.code = 400;
-      throw errorObj;
+      throw new CustomError("The '_id' query param must be a string.", 400);
     }
 
     if(numId && isNaN(parseInt(numId))){
-      let errorObj = new Error("The 'numId' must be a number.");
-      errorObj.code = 400;
-      throw errorObj;
+      throw new CustomError("The 'numId' must be a number.", 400);      
     }
 
     projections = typeof projections === 'string' ? JSON.parse(projections) : projections;
 
     if ((projections !== undefined) && !getIsObj(projections)) {
-      let errorObj = new Error("The 'projections' query param must be an object.");
-      errorObj.code = 400;
-      throw errorObj;
+      throw new CustomError("The 'projections' query param must be an object.", 400)
     }
 
     const { wasSuccessful, data, msg } = (projections ? await retrieveLessonsResultObj(_id, numId, projections) : await retrieveLessonsResultObj(_id, numId)) ?? {}
 
     if (!wasSuccessful && msg) {
-      let errorObj = new Error(msg);
-      errorObj.code = 500;
-      throw errorObj;
+      throw new CustomError(msg, 500);
     }
 
     if (!wasSuccessful) {
-      let errorObj = new Error("Failed to retrieve lessons from db.");
-      errorObj.code = 500;
-      throw errorObj;
+      throw new CustomError("Failed to retrieve lessons from db.", 500)
     }
 
     return response.status(200).json({ lessons: data });
   } catch (error) {
-    console.log('Error obj within catch block: ', error)
+    console.error('Error obj within catch block: ', error)
     const { code, message } = error;
 
     return response.status(code ?? 500).json({ msg: `Error message: ${message ?? 'An error has occurred on the server.'}` });
