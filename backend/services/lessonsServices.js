c418db1edc0d5b272e5b3fc2c1ffd2020ae154e3
Toriong <gtorion97@gmail.com>
2023-09-14 18:44:51 -0500

    creating update lesson path

--- backend/services/lessonsServices.js
+++ backend/services/lessonsServices.js
@@ -27,35 +27,54 @@ const deleteLesson = async lessonId => {
 
     try {
         await connectToMongodb();
-        
+
         await Lessons.deleteOne({ _id: lessonId })
 
         console.log(`Lesson with id ${lessonId} was successfully deleted from the database!`)
 
         return { status: 200, msg: 'Lesson was successfully deleted from the database!' }
     } catch (error) {
-        
+
         return { status: 500, msg: `Failed to delete lesson from the database. Error message: "${error}"` }
     }
 }
 
+// GOAL: query the database for the lesson that needs to be updated. Receive the following from the client:
+// - the filter object that will be used to update the given lesson
+// - the fields that will be updated: "key of the value that will be updated" : "the updated value"
+// - use $set to update the given field. 
 
+const updateLesson = async (filterObj, updatedValsAndKeysArr) => {
+    try {
+        const updatedLessonsKeysAndValsObj = Object.fromEntries(updatedValsAndKeysArr);
 
+        await Lessons.updateMany(filterObj, { $set: updatedLessonsKeysAndValsObj });
 
+        return {
+            msg: `The following fields were updated: ${updatedValsAndKeysArr.map(({ key }) => key).join(" ")}`,
+            wasSuccessful: true
+        }
+    } catch (error) {
+        const errMsg = `Failed to get the lesson from the database. Error message: ${error}.`;
 
+        console.error(errMsg)
+
+        return { wasSuccessful: false, msg: errMsg }
+    }
+}
 
 const retrieveLessonsResultObj = async (_id, numID, projectionObj = {}) => {
     try {
         let query = {};
-        
-        if(_id){
+
+        if (_id) {
             query._id = _id;
         }
-        
-        if(numID){
+
+        if (numID) {
             query.numID = parseInt(numID);
         }
-        
+
         const lessons = await Lessons.find(query, projectionObj).lean();
 
         return { wasSuccessful: true, data: lessons }
@@ -64,9 +83,13 @@ const retrieveLessonsResultObj = async (_id, numID, projectionObj = {}) => {
 
         console.error(errMsg)
 
-        return { wasSuccessful: false, msg: errMsg  }
+        return { wasSuccessful: false, msg: errMsg }
     }
-
 }
 
-export { insertLesson, deleteLesson, retrieveLessonsResultObj }
+export {
+    insertLesson,
+    deleteLesson,
+    retrieveLessonsResultObj,
+    updateLesson
+}
