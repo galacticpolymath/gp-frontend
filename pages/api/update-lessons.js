bebd2bc615099de349327b956a9351558c9c4d50
Toriong <gtorion97@gmail.com>
2023-09-18 19:20:47 -0500

    fixing eslint errors, wip

--- pages/api/update-lessons.js
+++ pages/api/update-lessons.js
@@ -1,23 +1,14 @@
-import { createFilterObj, retrieveLessonsResultObj, updateLesson } from "../../backend/services/lessonsServices";
-import { connectToMongodb } from "../../backend/utils/connection";
-
-// example of the filterObj: 
-// { numId: [1,2,3,4] }
-
-class CustomError {
-    constructor(message, code) {
-        this.message = message;
-        this.code = code;
-    }
-}
+import { createFilterObj, retrieveLessonsResultObj, updateLesson } from '../../backend/services/lessonsServices';
+import { connectToMongodb } from '../../backend/utils/connection';
+import { CustomError } from '../../backend/utils/errors';
 
 export default async function handler(request, response) {
-    try {
-        let { filterObj, keysAndUpdatedValsObj } = request.body;
-
-        if (filterObj && ((typeof filterObj !== 'object') || ((typeof filterObj === 'object') && (filterObj === null)))) {
-            throw new CustomError("The value for 'filterObj' field must be an object.", 400);
-        }
+  try {
+    const { filterObj, keysAndUpdatedValsObj } = request.body;
+    
+    if (filterObj && (typeof filterObj !== 'object' || (typeof filterObj === 'object' && filterObj === null))) {
+      throw new CustomError("The value for 'filterObj' field must be an object.",400,);
+    }
 
         if (!keysAndUpdatedValsObj) {
             throw new CustomError("'keysAndUpdatedValsObj' is required.", 400);
@@ -25,47 +16,65 @@ export default async function handler(request, response) {
 
         if (
             !keysAndUpdatedValsObj ||
-            (typeof keysAndUpdatedValsObj !== 'object') ||
-            ((typeof keysAndUpdatedValsObj === 'object') && ((keysAndUpdatedValsObj === null) || Array.isArray(keysAndUpdatedValsObj)))
+            typeof keysAndUpdatedValsObj !== 'object' ||
+            (typeof keysAndUpdatedValsObj === 'object' &&
+                (keysAndUpdatedValsObj === null || Array.isArray(keysAndUpdatedValsObj)))
         ) {
-            throw new CustomError("The field 'keysAndUpdatedValsObj' must be an object of all of updated values and their corresponding lesson keys.", 400);
+            throw new CustomError(
+                "The field 'keysAndUpdatedValsObj' must be an object of all of updated values and their corresponding lesson keys.",
+                400,
+            );
         }
 
         let filterObjForDbQuery = {};
-        filterObj = filterObj ? JSON.parse(JSON.stringify(filterObj)) : null;
-        keysAndUpdatedValsObj = JSON.parse(JSON.stringify(keysAndUpdatedValsObj));
+        const parsedFilterObj = filterObj ? JSON.parse(JSON.stringify(filterObj)) : null;
+        const parsedKeysAndUpdatedValsObj = JSON.parse(JSON.stringify(keysAndUpdatedValsObj));
 
-
-        if (filterObj) {
-            const { filterObj: _filterObjForUpdatedLessonServiceResult, errMsg: createFilterObjErrMsg } = createFilterObj(Object.entries(filterObj));
+        if (parsedFilterObj) {
+            const {
+                filterObj: _filterObjForUpdatedLessonServiceResult,
+                errMsg: createFilterObjErrMsg,
+            } = createFilterObj(Object.entries(parsedFilterObj));
 
             if (createFilterObjErrMsg) {
                 throw new CustomError(createFilterObjErrMsg, 400);
             }
 
-            filterObjForDbQuery = _filterObjForUpdatedLessonServiceResult
+            filterObjForDbQuery = _filterObjForUpdatedLessonServiceResult;
         }
 
         await connectToMongodb();
 
-        const { errMsg: updateLessonErrMsg } = await updateLesson(filterObjForDbQuery, keysAndUpdatedValsObj);
+        const { errMsg: updateLessonErrMsg } = await updateLesson(
+            filterObjForDbQuery,
+            parsedKeysAndUpdatedValsObj,
+        );
 
         if (updateLessonErrMsg) {
             throw new CustomError(updateLessonErrMsg, 400);
         }
 
-        const projectionForRetrieveUpdatedLessonsResultObj = Object.entries(keysAndUpdatedValsObj)
-            .map(keyAndVal => ([keyAndVal[0], 1]))
-            .reduce((projectionForRetrieveUpdatedLessonsResultObj, fieldAndProjectionNum) => {
+        const projectionForRetrieveUpdatedLessonsResultObj = Object.entries(
+            parsedKeysAndUpdatedValsObj,
+        )
+            .map(keyAndVal => [keyAndVal[0], 1])
+            .reduce((acc, fieldAndProjectionNum) => {
                 const [fieldName, projectionNum] = fieldAndProjectionNum;
-                projectionForRetrieveUpdatedLessonsResultObj[fieldName] = projectionNum;
+                acc[fieldName] = projectionNum;
 
-                return projectionForRetrieveUpdatedLessonsResultObj;
+                return acc;
             }, {});
-        const { data: lessons, errMsg: retrieveLessonsResultObjErrorMsg } = await retrieveLessonsResultObj(filterObjForDbQuery, projectionForRetrieveUpdatedLessonsResultObj)
+
+        const {
+            data: lessons,
+            errMsg: retrieveLessonsResultObjErrorMsg,
+        } = await retrieveLessonsResultObj(
+            filterObjForDbQuery,
+            projectionForRetrieveUpdatedLessonsResultObj,
+        );
 
         if (retrieveLessonsResultObjErrorMsg) {
-            throw new CustomError(retrieveLessonsResultObjErrorMsg, 500)
+            throw new CustomError(retrieveLessonsResultObjErrorMsg, 500);
         }
 
         const updatedLessonsResults = lessons.map(lesson => {
@@ -73,16 +82,16 @@ export default async function handler(request, response) {
 
             return {
                 _id: _id,
-                updatedFieldsObj: Object.keys(restOfLessonKeyValPairs)?.length ? restOfLessonKeyValPairs : null
-            }
-        })
+                updatedFieldsObj: Object.keys(restOfLessonKeyValPairs).length
+                    ? restOfLessonKeyValPairs
+                    : null,
+            };
+        });
 
-        return response.status(200).json({ updatedLessonsResults: updatedLessonsResults });
+        return response.status(200).json({ updatedLessonsResults });
     } catch (error) {
-        const { code, message } = error
-        console.log('error message: ', message)
+        const { code, message } = error;
 
         return response.status(code).json({ msg: message });
     }
 }
-
