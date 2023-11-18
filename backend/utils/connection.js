be2da9c98e04f32532fdcc3593decb5b523eb3c9
Toriong <gtorion97@gmail.com>
2023-11-17 21:17:07 -0600

    displaying individual lessons for each unit

--- backend/utils/connection.js
+++ backend/utils/connection.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import chalk from 'chalk';
 
 let isConnectedToDb = false;
 
@@ -9,12 +10,20 @@ export const connectToMongodb = async () => {
       return;
     }
 
-    const { MONGODB_PASSWORD, MONGODB_USER, MONGODB_DB_NAME } = process.env;
-    const connectionStr = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.tynope2.mongodb.net/${MONGODB_DB_NAME}`;
+    const { MONGODB_PASSWORD, MONGODB_USER, MONGODB_DB_NAME, MONGODB_DB_PROD_NAME } = process.env;
+    const dbName = MONGODB_DB_PROD_NAME ?? MONGODB_DB_NAME;
+    
+    if(MONGODB_DB_PROD_NAME){
+      console.log(chalk.red('YOU ARE ON PRODUCTION.'))
+    }
 
+    const connectionStr = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.tynope2.mongodb.net/${dbName}`;
+    console.log("connectionStr: ", connectionStr)
     const connectionState = await mongoose.connect(connectionStr, { retryWrites: true });
 
     isConnectedToDb = connectionState.connections[0].readyState === 1;
+
+    console.log("isConnectedToDb: ", isConnectedToDb)
 
     return { wasSuccessful: isConnectedToDb };
   } catch (error) {
