ec6e7ccc495ba72836b4b751139adf51497ff818
Toriong <gtorion97@gmail.com>
2024-01-18 18:44:46 -0600

    making purple notification section mobile responsive

--- components/LessonSection/SendFeedback.js
+++ components/LessonSection/SendFeedback.js
@@ -39,15 +39,15 @@ const SendFeedback = ({
             style={{ backgroundColor: "#EBD0FF", zIndex: 100 }}
             className={`w-100 position-absolute py-2 px-3 ${willHide ? "d-none" : "d-flex"}`}
         >
-            <section style={{ width: "2.5%" }} className="d-flex pt-3 pt-sm-0 justify-content-sm-center align-items-sm-center">
+            <section style={{ width: "2.5%" }} className="d-none d-sm-flex pt-3 pt-sm-0 justify-content-sm-center align-items-sm-center">
                 <i style={{ height: "fit-content" }} className="bi bi-tools increase-size-by-2x" />           
             </section>
-            <section className="px-3" style={{ width: "95%" }}>
+            <section className="px-sm-3" style={{ width: "95%" }}>
                 <span>
                     This unit is under construction. Please send your thoughts to <Link style={{ wordWrap: "break-word" }} className="no-link-decoration text-decoration-underline" href={`mailto:${FEEDBACK_EMAIL}`}>{FEEDBACK_EMAIL}</Link>! And be sure to <Link style={{ wordWrap: "break-word" }} className="no-link-decoration text-decoration-underline" href={SIGN_UP_FOR_EMAIL_LINK}>sign up for emails</Link> to get notified when the final version is released.
                 </span>
             </section>
-            <section style={{ width: "2.5%" }} className="d-flex pt-3 pt-sm-0 justify-content-sm-center align-items-sm-center">
+            <section style={{ width: "2.5%" }} className="d-none d-sm-flex pt-3 pt-sm-0 justify-content-sm-center align-items-sm-center">
                 <CloseBtnComp handleOnClick={handleOnClick} />
             </section>
         </div>
