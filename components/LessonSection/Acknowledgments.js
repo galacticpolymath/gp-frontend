c4cff3fe96444de511e7273cba28d56d2f46af56
Matt Wilkins <matt@galacticpolymath.com>
2024-01-19 19:08:45 -0600

    fixes to acknowledgements and standards formatting

--- components/LessonSection/Acknowledgments.js
+++ components/LessonSection/Acknowledgments.js
@@ -21,7 +21,7 @@ const Acknowledgments = ({
           <div key={i} className='mb-3'>
             <h6 className='fw-bolder'>{role}</h6>
             <RichText className=' text-black-87' content={def} />
-            {name &&records.map(({ name, url, title, affiliation, location }) => (
+            {name && records.map(({ name, url, title, affiliation, location }) => (
               <ul key={name}>
                 <li className='mt-3 fw-normal inline-block'>
                   <a
