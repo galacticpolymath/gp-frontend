13c36761a341c9a71afa49d8ab9c444168444979
Toriong <gtorion97@gmail.com>
2024-02-24 21:26:14 -0600

    using the service account to access google drive api

--- pages/api/download-all-files.js
+++ pages/api/download-all-files.js
@@ -1,12 +1,12 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable semi */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-empty */
 /* eslint-disable no-console */
 /* eslint-disable indent */
 /* eslint-disable no-multiple-empty-lines */
-import { authenticate } from '@google-cloud/local-auth';
-import { CustomError } from '../../backend/utils/errors';
 import fs from 'fs';
-
+import { authenticate } from '@google-cloud/local-auth';
 // NOTES:
 // will get tokens after the user has been authenticated with the google drive api
 
@@ -16,7 +16,7 @@ import fs from 'fs';
 
 // GOAL #5: download all of the lessons from the specified unit
 
-// GOAL #1: authenticate the user to google drive
+// GOAL #1: authorize the user to googleapis
 
 // SUB-GOAL: create the credentials json file
 
@@ -25,58 +25,47 @@ import fs from 'fs';
 const CREDENTIALS_PATH = 'credentials.json';
 const scopes = ['https://www.googleapis.com/auth/drive.readonly'];
 
-const createGoogleDriveCredentialsObj = () => {
-    const {
-        GOOGLE_DRIVE_CLIENT_SECRET,
-        GOOGLE_DRIVE_CLIENT_ID,
-        GOOGLE_DRIVE_PROJECT_ID,
-        GOOGLE_DRIVE_AUTH_URI,
-        GOOGLE_DRIVE_TOKEN_URI,
-        GOOGLE_DRIVE_AUTH_PROVIDER_x509_CERT_URL,
-        GOOGLE_DRIVE_REDIRECT_URIS,
-        GOOGLE_DRIVE_JAVASCRIPT_ORIGINS,
-    } = process.env;
-    const googleDriveRedirectUris = GOOGLE_DRIVE_REDIRECT_URIS.split(',');
-    const javascriptOrigins = GOOGLE_DRIVE_JAVASCRIPT_ORIGINS.split(',');
+class Credentials {
+    constructor() {
+        const {
+            GOOGLE_DRIVE_CLIENT_ID,
+            GOOGLE_DRIVE_PROJECT_ID,
+            GOOGLE_DRIVE_AUTH_URI,
+            GOOGLE_DRIVE_TOKEN_URI,
+            GOOGLE_DRIVE_AUTH_PROVIDER_x509_CERT_URL,
+            GOOGLE_DRIVE_CLIENT_SECRET,
+            GOOGLE_DRIVE_REDIRECT_URIS,
+        } = process.env
+        const redirectUris = GOOGLE_DRIVE_REDIRECT_URIS.split(',')
 
-    return {
-        'web': {
-            'client_id': GOOGLE_DRIVE_CLIENT_ID,
-            'project_id': GOOGLE_DRIVE_PROJECT_ID,
-            'auth_uri': GOOGLE_DRIVE_AUTH_URI,
-            'token_uri': GOOGLE_DRIVE_TOKEN_URI,
-            'auth_provider_x509_cert_url': GOOGLE_DRIVE_AUTH_PROVIDER_x509_CERT_URL,
-            'client_secret': GOOGLE_DRIVE_CLIENT_SECRET,
-            'redirect_uris': googleDriveRedirectUris,
-            'javascript_origins': javascriptOrigins,
-        },
-    };
-};
+        this.web = {
+            client_id: GOOGLE_DRIVE_CLIENT_ID,
+            project_id: GOOGLE_DRIVE_PROJECT_ID,
+            auth_uri: GOOGLE_DRIVE_AUTH_URI,
+            token_uri: GOOGLE_DRIVE_TOKEN_URI,
+            auth_provider_x509_cert_url: GOOGLE_DRIVE_AUTH_PROVIDER_x509_CERT_URL,
+            client_secret: GOOGLE_DRIVE_CLIENT_SECRET,
+            redirect_uris: redirectUris,
+        }
+    }
+}
 
 export default async function handler(request, response) {
     try {
-        if (request.method !== 'GET') {
-            throw new CustomError('Invalid request method.', 405);
-        }
+        const credentials = new Credentials();
 
-        const credentialsObj = createGoogleDriveCredentialsObj();
+        console.log('credentials, yo there meng: ', credentials)
 
-        console.log('credentialsObj, sup there: ', credentialsObj);
-
-        fs.writeFileSync(CREDENTIALS_PATH, JSON.stringify(credentialsObj));
+        fs.writeFileSync(CREDENTIALS_PATH, JSON.stringify(credentials))
 
         const client = await authenticate({
-            scopes,
-            keyfilePath: CREDENTIALS_PATH,
-        });
-
-        console.log('client, hey there meng: ', client);
+            scopes: scopes,
+            keyfilePath: CREDENTIALS_PATH
+        })
+        console.log('client: ', client)
 
         return response.json({ msg: 'GP lessons has been downloaded.' });
     } catch (error) {
-        console.error(error);
-
-        fs.unlinkSync(CREDENTIALS_PATH);
 
         return response.status(500).json({ msg: `Failed to download GP lessons. Reason: ${error}` });
     }
