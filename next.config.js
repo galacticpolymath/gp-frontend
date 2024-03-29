c0d8b57f9cc227f67401910c66bfe2c36c1d2222
Toriong <gtorion97@gmail.com>
2024-03-29 11:08:51 -0500

    updating the config file for csp

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const cspHeader = `
     font-src 'self';
     object-src 'none';
     base-uri 'self';
-    form-action http://localhost:3000/api/auth/signin/google https://dev.galacticpolymath.com/api/auth/signin/google https://galacticpolymath.com/api/auth/signin/google;
+    form-action 'self' http://localhost:3000/api/auth/signin/google https://dev.galacticpolymath.com/api/auth/signin/google https://galacticpolymath.com/api/auth/signin/google;
     connect-src 'self' https://oauth2.googleapis.com/token;
     block-all-mixed-content;
     upgrade-insecure-requests;
