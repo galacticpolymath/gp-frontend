e25df748fbfe38bea6ad2233f0eafa7878356afd
Toriong <gtorion97@gmail.com>
2024-05-30 22:10:34 -0500

    added releases carousel to home page

--- components/Home/NewRelease.js
+++ components/Home/NewRelease.js
@@ -9,26 +9,28 @@ const NewRelease = ({ newReleasePath, NewReleaseImage_src, sponsorImgPath, spons
         <div className='row justify-content-center'>
             <div className='pt-3 col-12 offset-0 offset-md-1 col-md-7 col-lg-6'>
                 <CustomLink href={newReleasePath} className='bg-danger no-link-decoration object-fit-contain w-auto'>
-                    <div className="position-relative mx-auto">
+                    <div className="position-relative mx-auto d-flex justify-content-center align-items-center d-md-block px-3 px-md-0">
                         <img
                             src={NewReleaseImage_src}
                             sizes="60vw"
                             className='lessonsPgShadow rounded-4 h-auto'
-                            style={{ objectFit: 'contain', height: "900px", width: "800px" }}
+                            style={{
+                                objectFit: 'contain',
+                            }}
                             alt="Newest release card"
                         />
-                        <div className='badge bg-secondary fs-6 text-center' style={{ zIndex: 15, position: 'absolute', top: '-10px', left: '-20px' }}>
+                        <div className='badge new-release-badge bg-secondary fs-6 text-center position-absolute'>
                             New release!
                         </div>
                     </div>
                 </CustomLink>
-                <div className='offset-3 mt-3'>
-                    <CustomLink href={newReleasePath} className='btn btn-primary '>
+                <div className='d-flex justify-content-center align-items-center mt-3'>
+                    <CustomLink href={newReleasePath} className='btn btn-primary px-2 px-sm-4 px-md-5 px-lg-4 see-this-lesson-btn'>
                         See this lesson
                     </CustomLink>
                 </div>
             </div>
-            <div className='col-12 col-md-3 col-lg-3 pt-3 pt-lg-5'>
+            <div className='col-12 col-md-3 col-lg-3 pt-3 pt-xxl-5'>
                 <h5 className='fw-light text-center'>Sponsor:</h5>
                 <div className=" w-80 mx-auto justify-content-center">
                     <img
@@ -39,7 +41,7 @@ const NewRelease = ({ newReleasePath, NewReleaseImage_src, sponsorImgPath, spons
                     />
                 </div>
                 <div className='my-3'>
-                    <h5 className='fw-light text-center text-wrap'> Dr. Emilie Snell-Rood&apos;s Lab at the University of Minnesota</h5>
+                    <h5 className='fw-light text-center text-wrap px-3 px-md-0'> Dr. Emilie Snell-Rood&apos;s Lab at the University of Minnesota</h5>
                 </div>
             </div>
         </div>
