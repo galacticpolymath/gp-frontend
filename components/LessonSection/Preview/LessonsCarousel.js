cce395a137cf2c7b69af46a3dafd1ac7d953dd5c
Toriong <gtorion97@gmail.com>
2024-08-07 15:27:10 -0500

    updating the sign in modal, inversing the order of the input fields

--- components/LessonSection/Preview/LessonsCarousel.js
+++ components/LessonSection/Preview/LessonsCarousel.js
@@ -120,6 +120,7 @@ const LessonsCarousel = ({ mediaItems }) => {
                         let src;
 
                         if ((type === 'video') || ((type === 'web-app') && webAppPreviewImg)) {
+                            console.log('mainLink, yo there! ', mainLink);
                             src = webAppPreviewImg ?? getVideoThumb(mainLink)
                         }
 
