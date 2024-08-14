dd8daf1c0abf46f679e3afa58c70d464de33c924
Toriong <gtorion97@gmail.com>
2024-08-14 17:10:07 -0500

    added bitly links, opened the gdrive files to preview when their image is clicked

--- pages/lessons/[loc]/[id]/index.js
+++ pages/lessons/[loc]/[id]/index.js
@@ -63,7 +63,6 @@ const addGradesOrYearsProperty = (sectionComps, ForGrades, GradesOrYears) => {
 }
 
 const LessonDetails = ({ lesson }) => {
-  console.log('lesson: ', lesson);
   const router = useRouter();
   const lessonSectionObjEntries = lesson?.Section ? Object.entries(lesson.Section) : [];
   let lessonStandardsIndexesToFilterOut = [];
