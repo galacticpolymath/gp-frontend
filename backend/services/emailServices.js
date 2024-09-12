d324fa18d6bbfb411a992d68478c4bf2c2812aea
Toriong <gtorion97@gmail.com>
2024-09-12 13:51:13 -0500

    incorporating gp official email creds

--- backend/services/emailServices.js
+++ backend/services/emailServices.js
@@ -3,21 +3,6 @@
 /* eslint-disable no-multiple-empty-lines */
 /* eslint-disable indent */
 import nodemailer from 'nodemailer';
-// import creds from '../../creds.json';
-
-// class EmailTransport {
-//     constructor() {
-//         const { EMAIL_USER, EMAIL_PASSWORD } = process.env;
-
-//         this.host = 'smtp.gmail.com';
-//         this.port = 465;
-//         this.secure = true;
-//         this.auth = {
-//             user: EMAIL_USER,
-//             pass: EMAIL_PASSWORD,
-//         };
-//     }
-// }
 
 /**
  * @typedef {Object} TMailOpts
@@ -34,11 +19,7 @@ import nodemailer from 'nodemailer';
  */
 export const sendEmail = async (mailOpts) => {
     try {
-        if (!process.env.SERVICE_ACCOUNT_PRIVATE_KEY) {
-            throw new Error("Service account private key environment variable is not present.");
-        }
-
-        const privateKey = process.env.SERVICE_ACCOUNT_PRIVATE_KEY.split("\\n").join("\n");
+        const privateKey = process.env.EMAIL_SENDER_SERVICE_ACCOUNT_PRIVATE_KEY.split("\\n").join("\n");
         const emailTransport = {
             host: 'smtp.gmail.com',
             port: 465,
@@ -47,35 +28,19 @@ export const sendEmail = async (mailOpts) => {
                 privateKey,
                 type: 'OAuth2',
                 user: "gabe@toriondev.com",
-                serviceClient: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+                serviceClient: process.env.EMAIL_SENDER_SERVICE_ACCOUNT_CLIENT_ID,
                 accessUrl: "https://oauth2.googleapis.com/token",
             },
         };
-        // console.log('emailTransport, yo there: ', emailTransport);
 
         const transport = nodemailer.createTransport(emailTransport);
-
-        // console.log("will verify transporter...", transport);
-
         const canSendEmail = await transport.verify();
-        console.log('bacon liver');
-        console.log('canSendEmail: ', canSendEmail);
 
-        // console.log('transporter has been verified.');
-
-        // const mailOpts = {
-        //     from: process.env.EMAIL_USER,
-        //     to: "gabe-948@ethereal-entity-414923.iam.gserviceaccount.com",
-        //     subject: 'Galactic Polymath Password Reset',
-        //     html: "<p>hi</p>",
-        // };
-
-        console.log('yo there!');
-
+        if (!canSendEmail) {
+            throw new Error("Email auth has failed.");
+        }
 
         const sentMessageInfo = await transport.sendMail(mailOpts);
-
-        // console.log()
 
         if (sentMessageInfo.rejected.length) {
             throw new Error('Failed to send the email to the target user.');
