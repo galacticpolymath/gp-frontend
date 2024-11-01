07d6d964acc5eecbebaa1616c1c8949e3efa1339
Toriong <gtorion97@gmail.com>
2024-10-31 18:56:15 -0700

    updated the errors file

--- backend/utils/errors.js
+++ backend/utils/errors.js
@@ -1,29 +1,35 @@
 class CustomError {
-  constructor(message, code = 500, type = '') {
+  constructor(message, code = 500, type = '', urlErrorParamKey = '', urlErrorParamVal) {
     this.message = message;
     this.code = code;
     this.type = type;
+    this.urlErrorParamKey = urlErrorParamKey;
+    this.urlErrorParamVal = urlErrorParamVal;
   }
 }
-class AuthError{
-  constructor(errType = 'failedAuthorizeUser', code = 500, redirectUrl = ''){
+class AuthError {
+  constructor(errType = 'failedAuthorizeUser', code = 500, redirectUrl = '', urlErrorParamKey = '', urlErrorParamVal = '') {
     this.errType = errType;
     this.code = code;
     this.redirectUrl = redirectUrl;
+    this.urlErrorParamKey = urlErrorParamKey;
+    this.urlErrorParamVal = urlErrorParamVal;
   }
 }
-class AuthMiddlwareError{
-  constructor(isAuthorize, errResponse, msg){
+class AuthMiddlwareError {
+  constructor(isAuthorize, errResponse, msg) {
     this.isAuthorize = isAuthorize;
     this.msg = msg;
     this.errResponse = errResponse;
   }
 }
-class SignInError{
-  constructor(type = 'sign-in-error', msg = '',code = 500){
+class SignInError {
+  constructor(type = 'sign-in-error', msg = '', code = 500, urlErrorParamKey = '', urlErrorParamVal) {
     this.type = type;
     this.code = code;
     this.msg = msg;
+    this.urlErrorParamKey = urlErrorParamKey;
+    this.urlErrorParamVal = urlErrorParamVal;
   }
 }
 
