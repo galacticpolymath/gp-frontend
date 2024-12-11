afd8efb57cae32600f581ff8f46992948204f3e7
Toriong <gtorion97@gmail.com>
2024-12-11 10:22:49 -0800

    using different version of node, updating package lock json file

--- .github/checkDeadLinks.js
+++ .github/checkDeadLinks.js
@@ -1,43 +1,175 @@
-import { LinkChecker } from 'linkinator';
+import { LinkChecker } from "linkinator";
+import ExcelJS from "exceljs";
+import fs from "fs";
+import { sendEmailWithRetries } from "../backend/services/emailServices";
+
+const EXCEL_SHEET_FILE_NAME = "DeadLinksCheckResult.xlsx";
+
+const formatNum = (num) => (num.toString().length === 1 ? `0${num}` : `${num}`);
+
+const getCurrentDate = (
+    isMilitary = false,
+    separator = ".",
+    willGetOnlyDate = false
+) => {
+    const dateObj = new Date();
+    const month = formatNum(dateObj.getUTCMonth() + 1);
+    const day = formatNum(dateObj.getUTCDay() + 1);
+    const year = dateObj.getFullYear();
+
+    if (willGetOnlyDate) {
+        return `${month}${separator}${day}${separator}${year}`;
+    }
+
+    let hours = dateObj.getHours();
+    hours = formatNum(hours > 12 ? hours - 12 : hours);
+    const minutes = formatNum(dateObj.getMinutes());
+
+    if (isMilitary) {
+        return `${month}${separator}${day}${separator}${year} ${hours}:${minutes}`;
+    }
+
+    const timeOfDay = dateObj.getHours() > 12 ? "pm" : "am";
+
+    return `${month}${separator}${day}${separator}${year} ${hours}:${minutes}${timeOfDay}`;
+};
+
+/**
+ * Generates an Excel worksheet summarizing the results of a dead links check.
+ *
+ * @param {{ parent: string, url: string, code: number }[]} deadLinks - An array of dead link objects, each containing information about the link.
+ *
+ * This function creates an Excel worksheet with a title that includes the
+ * date of the check. It adds column headers for "Checked Link", "Link Location",
+ * and "Status", and styles them with bold font and centered alignment. The
+ * columns are set to a specific width to accommodate the data.
+ */
+async function createDeadLinksResultsExcelSheet(deadLinks, currentDateStr) {
+    console.log("will createt dead links results");
+    console.log("deadLinks length: ", deadLinks.length);
+    const workBook = new ExcelJS.Workbook();
+    const workSheetTitle = `Dead links check on ${getCurrentDate(
+        true,
+        ".",
+        true
+    )}`;
+    console.log("workSheetTitle: ", workSheetTitle);
+    const worksheet = workBook.addWorksheet(workSheetTitle);
+    console.log("workBook: ", workBook);
+    worksheet.mergeCells("A1:B1:C1");
+    const titleCell = worksheet.getCell("A1");
+    titleCell.value = `Dead links check on ${currentDateStr}`;
+    titleCell.font = { size: 20, bold: true };
+
+    // Add column headers
+    worksheet.getCell("A2").value = "Checked Link";
+    worksheet.getCell("B2").value = "Link Location";
+    worksheet.getCell("C2").value = "Status";
+    worksheet.getCell("D2").value = "Code";
+
+    // Style column headers
+    ["A2", "B2", "C2", "D2"].forEach((cell) => {
+        worksheet.getCell(cell).font = { size: 15, bold: true };
+    });
+
+    worksheet.columns = [{ width: 70 }, { width: 70 }];
+
+    for (const [index, { code, url, parent }] of deadLinks.entries()) {
+        const row = index + 3;
+        const statusCell = worksheet.getCell(`C${row}`);
+        worksheet.getCell(`A${row}`).value = url;
+        worksheet.getCell(`B${row}`).value = parent;
+        statusCell.value = "BROKEN";
+        statusCell.font = { color: { argb: "ff0000" } };
+        worksheet.getCell(`D${row}`).value = code;
+    }
+
+    try {
+        workBook.xlsx.writeFile(EXCEL_SHEET_FILE_NAME);
+    } catch (error) {
+        console.log("Error creating Excel file: ", error);
+    }
+}
 
 async function checkForDeadLinks() {
-    // create a new `LinkChecker` that we'll use to run the scan.
     const checker = new LinkChecker();
 
     // Respond to the beginning of a new page being scanned
-    checker.on('pagestart', url => {
+    checker.on("pagestart", (url) => {
         console.log(`Scanning ${url}`);
     });
 
-    // After a page is scanned, check out the results!
-    checker.on('link', result => {
-        // check the specific url that was scanned
+    // Respond to a link being scanned
+    checker.on("link", (result) => {
         console.log(`link of page:  ${result.parent}`);
         console.log(`url scanned:  ${result.url}`);
         console.log(`status code:  ${result.status}`);
         console.log(`link state:  ${result.state}`);
     });
 
-    // Go ahead and start the scan! As events occur, we will see them above.
+    // use the prod url to check the urls
+    // after the check has been completed, do the following: 
+    // -get the url in the parent value
+    // -pass it as a value for the url constructor class 
+    // -get the pathname from the url object
+    // -using the dev url append the path to it
+    // -insert the final string for the parent property of the object that contains the results 
     const result = await checker.check({
-        path: 'https://dev.galacticpolymath.com/',
+        path: "https://dev.galacticpolymath.com/",
         recurse: true,
-        timeout: 8_000,
-        retry: true,
+        timeout: 7_000,
         retryErrors: true,
         retryErrorsCount: 3,
         retryErrorsJitter: 5,
     });
 
-    // Check to see if the scan passed!
-    console.log(result.passed ? 'PASSED :D' : 'FAILED :(');
+    console.log(result.passed ? "PASSED :D" : "FAILED :(");
 
-    // How many links did we scan?
     console.log(`Scanned total of ${result.links.length} links!`);
 
-    // The final result will contain the list of checked links, and the pass/fail
-    const brokeLinksCount = result.links.filter(link => link.state === 'BROKEN');
-    console.log(`Detected ${brokeLinksCount.length} broken links.`);
+    return result.links.filter((link) => link.state === "BROKEN");
 }
 
-checkForDeadLinks();
+(async () => {
+    try {
+        const deadLinks = await checkForDeadLinks();
+        const currentDateStr = getCurrentDate(true, "/");
+        const text = deadLinks.length
+            ? `A git push has been made. Dead links check has been executed. There ${deadLinks.length == 1 ? "is" : "are"
+            } ${deadLinks.length} dead ${deadLinks.length == 1 ? "link" : "links"
+            }. See attached file.`
+            : "A git push has been made. Dead links check has been executed. No dead links found.";
+        let attachments = [];
+
+        if (deadLinks.length) {
+            await createDeadLinksResultsExcelSheet(
+                deadLinks,
+                currentDateStr
+            );
+            attachments = [
+                {
+                    filename: EXCEL_SHEET_FILE_NAME,
+                    path: EXCEL_SHEET_FILE_NAME,
+                },
+            ];
+        }
+
+        const { wasSuccessful } = await sendEmailWithRetries({
+            from: "shared@galacticpolymath.com",
+            to: "gtorionnotion@gmail.com",
+            subject: `Deads Links Check On ${currentDateStr}`,
+            text,
+            attachments,
+        });
+
+        if (!wasSuccessful) {
+            throw new Error("Email failed to be sent.");
+        }
+    } catch (error) {
+        console.log("Error checking for dead links: ", error);
+    } finally {
+        if (fs.existsSync(EXCEL_SHEET_FILE_NAME)) {
+            fs.unlinkSync(EXCEL_SHEET_FILE_NAME);
+        }
+    }
+})();
--- backend/services/emailServices.js
+++ backend/services/emailServices.js
@@ -115,6 +115,38 @@ export const addUserToEmailList = async (email, clientUrl) => {
     }
 };
 
+
+/**
+ * Sends an email with the given options and retries the operation if it fails.
+ * Retries are done with an exponential backoff.
+ * @param {TMailOpts} mailOpts
+ * @param {number} [retries=1]
+ * @return {Promise<{ wasSuccessful: boolean }>} A promise that resolves to an object with a boolean indicating whether the operation was successful.
+ */
+export const sendEmailWithRetries = async (mailOpts, retries = 1) => {
+    try {
+        const { wasSuccessful } = await sendEmail(mailOpts)
+
+        if (!wasSuccessful) {
+            throw new Error("Failed to send email. Retrying...");
+        }
+
+        return { wasSuccessful: true };
+    } catch (error) {
+        if (retries <= 3) {
+            const randomNumMs = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+            const waitTime = randomNumMs + (retries * 1_000);
+            console.log("Will wait for ", waitTime);
+            await new Promise((resolve) => setTimeout(resolve, waitTime));
+            return await sendEmailWithRetries(mailOpts, retries + 1);
+        }
+
+        console.error("Failed to send the email.")
+
+        return { wasSuccessful: false };
+    }
+};
+
 /**
  * @param {string} email The email of the user who will be deletede from the mailing list. 
  * @return {Promise<{ wasSuccessful: boolean }>} A promise that resolves to an object with a boolean indicating whether the operation was successful.
