6d4415fc2d8d6c96f9dcde2f0caf6f79b8df9122
Toriong <gtorion97@gmail.com>
2024-12-17 19:57:51 -0800

    increase timeout for auth route

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth/next';
 import { authOptions } from '../../../backend/authOpts/authOptions';
 
 export const config = {
-  maxDuration: 40,
+  maxDuration: 45,
 };
 
 export default NextAuth(authOptions);
