2d09231973eee46feb57cc30796e5bd61329a56c
Toriong <gtorion97@gmail.com>
2025-01-13 16:36:03 -0800

    updated packages

--- pages/api/get-brevo-status.js
+++ pages/api/get-brevo-status.js
@@ -1,51 +1,58 @@
+/* eslint-disable quotes */
 
 /**
  * Handles incoming API requests to retrieve the Brevo status.
- * 
+ *
  * @param {import('next').NextApiRequest} request - The incoming request object.
  * @param {import('next').NextApiResponse} response - The response object to send data back.
  */
 
-import MailingListConfirmations from '../../backend/models/mailingListConfirmation';
-import { getMailingListContact } from '../../backend/services/emailServices';
-import { deleteMailingListConfirmationsByEmail } from '../../backend/services/mailingListConfirmationServices';
+import MailingListConfirmations from "../../backend/models/mailingListConfirmation";
+import { getMailingListContact } from "../../backend/services/emailServices";
+import { deleteMailingListConfirmationsByEmail } from "../../backend/services/mailingListConfirmationServices";
 
 export default async function handler(request, response) {
-    try {
-        if (request.method !== 'GET') {
-            throw new Error("Incorrect request method. Must be a 'GET'.");
-        }
-
-        if (!request.query || !request.query.mailingListConfirmationId) {
-            throw new Error("The 'mailingListConfirmationId' param is not found in the request param.");
-        }
-
-        const mailingListConfirmationIdDoc = await MailingListConfirmations.findOne({ _id: request.query.mailingListConfirmationId }).lean();
-
-        console.log("mailingListConfirmationIdDoc: ", mailingListConfirmationIdDoc);
-
-        if (!mailingListConfirmationIdDoc || !mailingListConfirmationIdDoc.email) {
-            throw new Error("The target mailing list confirmation document does not exist.");
-        }
-
-
-        const mailingListContactPromise =
-            getMailingListContact(mailingListConfirmationIdDoc.email);
-        const deleteMailingListConfirmationByEmailPromise =
-            deleteMailingListConfirmationsByEmail(mailingListConfirmationIdDoc.email);
-        const [mailingListContact,] = await Promise.all([
-            mailingListContactPromise,
-            deleteMailingListConfirmationByEmailPromise
-        ]);
-
-        if (!mailingListContact) {
-            throw new Error("The target user was not found on the mailing list.");
-        }
-
-        return response.status(200).json({ isOnMailingList: true });
-    } catch (error) {
-        console.error("An error has occurred. Failed to retrieve the brevo status of the non-signed in user. Reason: ", error);
-
-        return response.status(500).json({ isOnMailingList: false, errType: "userNotFound" });
+  try {
+    if (request.method !== "GET") {
+      throw new Error("Incorrect request method. Must be a 'GET'.");
     }
+
+    if (!request.query || !request.query.mailingListConfirmationId) {
+      throw new Error(
+        "The 'mailingListConfirmationId' param is not found in the request param."
+      );
+    }
+
+    const mailingListConfirmationIdDoc = await MailingListConfirmations.findOne(
+      { _id: request.query.mailingListConfirmationId }
+    ).lean();
+
+    if (!mailingListConfirmationIdDoc || !mailingListConfirmationIdDoc.email) {
+      throw new Error(
+        "The target mailing list confirmation document does not exist."
+      );
+    }
+
+    const mailingListContactPromise = getMailingListContact(
+      mailingListConfirmationIdDoc.email
+    );
+    const deleteMailingListConfirmationByEmailPromise =
+      deleteMailingListConfirmationsByEmail(mailingListConfirmationIdDoc.email);
+    const [mailingListContact] = await Promise.all([
+      mailingListContactPromise,
+      deleteMailingListConfirmationByEmailPromise,
+    ]);
+
+    if (!mailingListContact) {
+      throw new Error("The target user was not found on the mailing list.");
+    }
+
+    return response.status(200).json({ isOnMailingList: true });
+  } catch (error) {
+    const errMsg = `An error has occurred. Failed to retrieve the brevo status of the non-signed in user. Reason: ${error}`;
+
+    return response
+      .status(500)
+      .json({ isOnMailingList: false, errType: "userNotFound", errMsg });
+  }
 }
--- pages/api/get-signed-in-user-brevo-status.js
+++ pages/api/get-signed-in-user-brevo-status.js
@@ -1,33 +1,50 @@
+/* eslint-disable quotes */
+
 import { getMailingListContact } from "../../backend/services/emailServices";
 import { CustomError } from "../../backend/utils/errors";
 import { verifyJwt } from "../../nondependencyFns";
 
 export default async function handler(request, response) {
-    try {
-        console.log('what is up there, request: ', request);
+  try {
+    console.log("what is up there, request: ", request);
 
-        if (request.method !== 'GET') {
-            throw new CustomError("Incorrect request method. Must be a 'GET'.", 405, 'incorrectMethod');
-        }
-
-        const token = request.headers.authorization.replace('Bearer ', '');
-        const { payload } = await verifyJwt(token);
-
-        if (!payload) {
-            throw new CustomError("You are not authorized to access this service. Token invalid.", 401, 'invalidToken');
-        };
-
-        if (!payload.email) {
-            throw new CustomError("You are not authorized to access this service. No email found in the token.", 401, 'invalidTokenEmailNotFound');
-        };
-
-        const emailListContact = await getMailingListContact(payload.email);
-
-        return response.status(200).json({ isOnMailingList: !!emailListContact });
-    } catch (error) {
-        const { errType, msg } = error ?? {}
-        console.error("Unable to get the mailing list status of the signed user. Reason: ", msg ?? error);
-
-        return response.status(500).json({ errType, msg, isOnMailingList: false });
+    if (request.method !== "GET") {
+      throw new CustomError(
+        "Incorrect request method. Must be a 'GET'.",
+        405,
+        "incorrectMethod"
+      );
     }
+
+    const token = request.headers.authorization.replace("Bearer ", "");
+    const { payload } = await verifyJwt(token);
+
+    if (!payload) {
+      throw new CustomError(
+        "You are not authorized to access this service. Token invalid.",
+        401,
+        "invalidToken"
+      );
+    }
+
+    if (!payload.email) {
+      throw new CustomError(
+        "You are not authorized to access this service. No email found in the token.",
+        401,
+        "invalidTokenEmailNotFound"
+      );
+    }
+
+    const emailListContact = await getMailingListContact(payload.email);
+
+    return response.status(200).json({ isOnMailingList: !!emailListContact });
+  } catch (error) {
+    const { errType, msg } = error ?? {};
+    console.error(
+      "Unable to get the mailing list status of the signed user. Reason: ",
+      msg ?? error
+    );
+
+    return response.status(500).json({ errType, msg, isOnMailingList: false });
+  }
 }
