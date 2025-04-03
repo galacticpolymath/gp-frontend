9da448e74abfca9f1a52242873c7d0e1d05d0afc
Toriong <gtorion97@gmail.com>
2025-04-02 20:11:13 -0700

    new CRUD routes added, added types to the teach it section

--- pages/api/get-units.ts
+++ pages/api/get-units.ts
@@ -1,31 +1,68 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { connectToMongodb } from '../../backend/utils/connection';
-import { CustomError } from '../../backend/utils/errors';
-import { createDbFilter, retrieveUnits, TProjections } from '../../backend/services/unitServices';
-import { IUnit } from '../../backend/models/Unit/types/unit';
+/* eslint-disable quotes */
 
-export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToMongodb } from "../../backend/utils/connection";
+import { CustomError } from "../../backend/utils/errors";
+import {
+  createDbFilter,
+  retrieveUnits,
+  TProjections,
+} from "../../backend/services/unitServices";
+import { IUnit } from "../../backend/models/Unit/types/unit";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
   try {
     const { method, query } = request;
 
-    if (method !== 'GET') {
-      throw new CustomError('This route only accepts GET requests.', 404);
+    if (method !== "GET") {
+      throw new CustomError("This route only accepts GET requests.", 404);
     }
 
-    const { filterObj, projectionsObj } = (query ?? {}) as { filterObj: string, projectionsObj: string  };
+    const { filterObj, projectionsObj } = (query ?? {}) as {
+      filterObj: string;
+      projectionsObj: string;
+    };
 
-    const dbProjections: unknown = (typeof projectionsObj === 'string') ? JSON.parse(projectionsObj) : projectionsObj;
-    const dbFilter: unknown = (typeof filterObj === 'string') ? JSON.parse(filterObj) : filterObj;
+    const dbProjections: unknown =
+      typeof projectionsObj === "string"
+        ? JSON.parse(projectionsObj)
+        : projectionsObj;
+    const dbFilter: unknown =
+      typeof filterObj === "string" ? JSON.parse(filterObj) : filterObj;
 
-    if (dbProjections && ((typeof dbProjections !== 'object') && (dbProjections === null) || Array.isArray(dbProjections) || (typeof dbProjections !== 'object'))) {
-      throw new CustomError('`projectionsObj` must be an non-array object.', 400);
+    if (
+      dbProjections &&
+      ((typeof dbProjections !== "object" && dbProjections === null) ||
+        Array.isArray(dbProjections) ||
+        typeof dbProjections !== "object")
+    ) {
+      throw new CustomError(
+        "`projectionsObj` must be an non-array object.",
+        400
+      );
     }
 
-    if (dbFilter && ((typeof dbFilter !== 'object') && (dbFilter === null) || Array.isArray(dbFilter) || (typeof dbFilter !== 'object'))) {
-      throw new CustomError('`dbFilter` must be an non-array object. Example: { numID: [1,2,3,4] }', 400);
+    if (
+      dbFilter &&
+      ((typeof dbFilter !== "object" && dbFilter === null) ||
+        Array.isArray(dbFilter) ||
+        typeof dbFilter !== "object")
+    ) {
+      throw new CustomError(
+        "`dbFilter` must be an non-array object. Example: { numID: [1,2,3,4] }",
+        400
+      );
     }
-    const dbFilterEntries = Object.entries(dbFilter as Record<string, unknown[]>);
-    const dbFilterCreationResult = (dbFilter && Object.keys(dbFilter).length) ? createDbFilter(dbFilterEntries) : null;
+    const dbFilterEntries = Object.entries(
+      dbFilter as Record<string, unknown[]>
+    );
+    const dbFilterCreationResult =
+      dbFilter && Object.keys(dbFilter).length
+        ? createDbFilter(dbFilterEntries)
+        : null;
 
     if (dbFilterCreationResult?.errMsg) {
       throw new CustomError(dbFilterCreationResult.errMsg, 400);
@@ -34,14 +71,17 @@ export default async function handler(request: NextApiRequest, response: NextApi
     const { wasSuccessful: wasConnectionSuccessful } = await connectToMongodb(
       15_000,
       0,
-      true,
+      true
     );
 
     if (!wasConnectionSuccessful) {
-      throw new CustomError('Failed to connect to the database.', 500);
+      throw new CustomError("Failed to connect to the database.", 500);
     }
 
-    const { data, errMsg } = await retrieveUnits(dbFilterCreationResult?.filterObj as Record<keyof IUnit, unknown>, dbProjections as TProjections);
+    const { data, errMsg } = await retrieveUnits(
+      dbFilterCreationResult?.filterObj as Record<keyof IUnit, unknown>,
+      dbProjections as TProjections
+    );
 
     if (errMsg) {
       throw new CustomError(errMsg, 500);
@@ -49,10 +89,19 @@ export default async function handler(request: NextApiRequest, response: NextApi
 
     return response.status(200).json({ lessons: data });
   } catch (error: unknown) {
-    const { code: errorCode, message: errorMessage }: { code?: number, message?: string } = error ?? {};
+    const {
+      code: errorCode,
+      message: errorMessage,
+    }: { code?: number; message?: string } = error ?? {};
     const code = errorCode ?? 500;
-    const message = errorMessage ?? 'An error has occurred on the server.';
+    const message = errorMessage ?? "An error has occurred on the server.";
 
-    return response.status(code ?? 500).json({ msg: `Error message: ${message ?? 'An error has occurred on the server.'}` });
+    return response
+      .status(code ?? 500)
+      .json({
+        msg: `Error message: ${
+          message ?? "An error has occurred on the server."
+        }`,
+      });
   }
 }
--- pages/api/insert-unit.ts
+++ pages/api/insert-unit.ts
@@ -1,30 +1,39 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { insertLesson } from '../../backend/services/lessonsServices';
-import { connectToMongodb } from '../../backend/utils/connection';
-import { CustomError } from '../../backend/utils/errors';
-import { INewUnitSchema } from '../../backend/models/Unit/types/unit';
+/* eslint-disable quotes */
 
-export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+import { NextApiRequest, NextApiResponse } from "next";
+import { insertLesson } from "../../backend/services/lessonsServices";
+import { connectToMongodb } from "../../backend/utils/connection";
+import { CustomError } from "../../backend/utils/errors";
+import { INewUnitSchema } from "../../backend/models/Unit/types/unit";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
   try {
     if (
       !request?.body?.unit ||
-      (request?.body?.unit && (typeof request?.body?.unit === 'object') && !Object.keys(request?.body?.unit)?.length) ||
-      (typeof request?.body?.unit !== 'object')
+      (request?.body?.unit &&
+        typeof request?.body?.unit === "object" &&
+        !Object.keys(request?.body?.unit)?.length) ||
+      typeof request?.body?.unit !== "object"
     ) {
-      console.error('Invalid request: The `request.body.unit` is either empty or not of the correct data type.');
+      console.error(
+        "Invalid request: The `request.body.unit` is either empty or not of the correct data type."
+      );
 
-      return response.status(400).json({ msg: 'The `request.body.unit` is empty or the wrong data type.' });
+      return response
+        .status(400)
+        .json({
+          msg: "The `request.body.unit` is empty or the wrong data type.",
+        });
     }
 
     const newUnit: INewUnitSchema = request?.body?.unit;
-    const { wasSuccessful } = await connectToMongodb(
-      15_000,
-      0,
-      true
-    );
+    const { wasSuccessful } = await connectToMongodb(15_000, 0, true);
 
     if (!wasSuccessful) {
-      throw new CustomError('Failed to connect to the database.', 500);
+      throw new CustomError("Failed to connect to the database.", 500);
     }
 
     const { status, msg } = await insertLesson(newUnit);
@@ -35,8 +44,10 @@ export default async function handler(request: NextApiRequest, response: NextApi
 
     return response.status(status).json({ msg: msg });
   } catch (error: unknown) {
-    const { code, message } = error as { code: number, message: string };
+    const { code, message } = error as { code: number; message: string };
 
-    return response.status(code ?? 500).json({ msg: message ?? 'Failed to insert lesson into the db.' });
+    return response
+      .status(code ?? 500)
+      .json({ msg: message ?? "Failed to insert lesson into the db." });
   }
 }
