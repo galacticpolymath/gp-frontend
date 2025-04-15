244a7f3664d7696006d798a2fffabaed8869ee4c
Toriong <gtorion97@gmail.com>
2025-04-15 14:58:19 -0700

    added types to useCustomCookies hook

---
+++ customHooks/useCustomCookies.tsx
@@ -0,0 +1,20 @@
+import { useCookies } from "react-cookie";
+
+export const useCustomCookies = (keys = ["token"]) => {
+  const [cookiesStore, setCookie, removeCookie] = useCookies(keys);
+
+  const clearCookies = () => {
+    const cookies = Object.keys(cookiesStore);
+
+    for (const cookieKey of cookies) {
+      removeCookie(cookieKey);
+    }
+  };
+
+  return {
+    cookies: cookiesStore,
+    setCookie,
+    removeCookie,
+    clearCookies,
+  };
+};
