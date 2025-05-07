55ca9ff953f9f509e47c7eb27a430ede1fca11b2
Toriong <gtorion97@gmail.com>
2025-05-06 17:34:25 -0700

    sparkle added, app able to be built

--- components/SparklesAnimation.tsx
+++ components/SparklesAnimation.tsx
@@ -1,16 +1,15 @@
-import styled, { keyframes } from "styled-components";
-import { useRandomInterval } from "../customHooks/useInterval";
-import React, { CSSProperties, useState } from "react";
-import { usePrefersReducedMotion } from "../customHooks/usePrefersReducedMotions";
-import { random } from "../shared/fns";
+import styled, { keyframes } from 'styled-components';
+import { useRandomInterval } from '../customHooks/useInterval';
+import React, { CSSProperties, useState } from 'react';
+import { usePrefersReducedMotion } from '../customHooks/usePrefersReducedMotions';
+import { random } from '../shared/fns';
 
-const DEFAULT_COLOR = "#6812D1";
 const COLOR_OPTS = {
-  purple: "#6812D1",
-  pink: "#ff3DAC",
+  purple: '#6812D1',
+  pink: '#ff3DAC',
 } as const;
 
-type TSparkleColor = "#6812D1" | "#ff3DAC";
+type TSparkleColor = '#6812D1' | '#ff3DAC';
 
 const generateSparkle = (color: TSparkleColor) => {
   const sparkle = {
@@ -19,8 +18,8 @@ const generateSparkle = (color: TSparkleColor) => {
     color,
     size: random(10, 20),
     style: {
-      top: random(0, 100) + "%",
-      left: random(0, 100) + "%",
+      top: `${random(0, 100)}%`,
+      left: `${random(0, 100)}%`,
     },
   };
 
@@ -32,6 +31,74 @@ interface IProps {
   children?: React.ReactNode;
   sparkleWrapperStyle: CSSProperties;
 }
+
+const Wrapper = styled.span`
+  display: inline-block;
+  position: relative;
+`;
+
+const comeInOut = keyframes`
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+`;
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(180deg);
+  }
+`;
+
+const SparkleWrapper = styled.span`
+  position: absolute;
+  display: block;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${comeInOut} 700ms forwards;
+  }
+`;
+
+const SparkleSvg = styled.svg`
+  display: block;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${spin} 100ms linear;
+  }
+`;
+
+const ChildWrapper = styled.strong`
+  position: relative;
+  z-index: 1;
+  font-weight: bold;
+`;
+
+const Sparkle = ({ size, color, style }: any) => {
+  const path =
+    'M26.5 25.5C19.0043 33.3697 0 34 0 34C0 34 19.1013 35.3684 26.5 43.5C33.234 50.901 34 68 34 68C34 68 36.9884 50.7065 44.5 43.5C51.6431 36.647 68 34 68 34C68 34 51.6947 32.0939 44.5 25.5C36.5605 18.2235 34 0 34 0C34 0 33.6591 17.9837 26.5 25.5Z';
+
+  return (
+    <SparkleWrapper style={style}>
+      <SparkleSvg
+        width={size}
+        height={size}
+        viewBox="0 0 68 68"
+        fill="none"
+      >
+        <path d={path} fill={color} />
+      </SparkleSvg>
+    </SparkleWrapper>
+  );
+};
 
 const Sparkles = ({
   color,
@@ -45,7 +112,7 @@ const Sparkles = ({
     );
   });
 
-  const prefersReducedMotion = usePrefersReducedMotion();
+  usePrefersReducedMotion();
 
   useRandomInterval(
     () => {
@@ -80,68 +147,5 @@ const Sparkles = ({
     </Wrapper>
   );
 };
-
-const Sparkle = ({ size, color, style }: any) => {
-  const path =
-    "M26.5 25.5C19.0043 33.3697 0 34 0 34C0 34 19.1013 35.3684 26.5 43.5C33.234 50.901 34 68 34 68C34 68 36.9884 50.7065 44.5 43.5C51.6431 36.647 68 34 68 34C68 34 51.6947 32.0939 44.5 25.5C36.5605 18.2235 34 0 34 0C34 0 33.6591 17.9837 26.5 25.5Z";
-
-  return (
-    <SparkleWrapper style={style}>
-      <SparkleSvg width={size} height={size} viewBox="0 0 68 68" fill="none">
-        <path d={path} fill={color} />
-      </SparkleSvg>
-    </SparkleWrapper>
-  );
-};
-
-const comeInOut = keyframes`
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1);
-  }
-  100% {
-    transform: scale(0);
-  }
-`;
-
-const spin = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-
-  100% {
-    transform: rotate(180deg);
-  }
-`;
-
-const Wrapper = styled.span`
-  display: inline-block;
-  position: relative;
-`;
-
-const SparkleWrapper = styled.span`
-  position: absolute;
-  display: block;
-
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${comeInOut} 700ms forwards;
-  }
-`;
-
-const SparkleSvg = styled.svg`
-  display: block;
-
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${spin} 100ms linear;
-  }
-`;
-
-const ChildWrapper = styled.strong`
-  position: relative;
-  z-index: 1;
-  font-weight: bold;
-`;
 
 export default Sparkles;
