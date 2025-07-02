29db778bcfae6ce8aeebcb64e91ce96490a1d98f
Toriong <gtorion97@gmail.com>
2025-07-01 19:08:51 -0700

    added the blog post link for the soundlab app

---
+++ components/CustomLink.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable indent */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-indent-props */
+
+import React from 'react';
+import Link from 'next/link';
+import { ReactNode } from 'react';
+
+interface IProps {
+    hrefStr?: string;
+    children?: ReactNode;
+    fontSize?: string | number;
+    targetLinkStr?: string;
+    color?: string;
+    style?: React.CSSProperties;
+    className?: string;
+}
+
+const CustomLink = ({
+    hrefStr = '',
+    children,
+    fontSize = undefined,
+    targetLinkStr = '_self',
+    color = '',
+    style = {},
+    className = 'no-link-decoration',
+}: IProps) => {
+    if (color) {
+        style.color = color;
+    }
+    if (fontSize) {
+        style.fontSize = fontSize;
+    }
+
+    return (
+        <Link
+            style={style}
+            href={hrefStr}
+            target={targetLinkStr}
+            rel="noopener noreferrer"
+            className={className}
+        >
+            {children}
+        </Link>
+    );
+};
+
+export default CustomLink;
