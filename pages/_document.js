06d79a679eae7a0dd3e4319d8a3d928b6be2b7fd
Toriong <gtorion97@gmail.com>
2025-07-09 19:51:16 -0700

    added the _document file

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var o_options = {
+                domain: 'galactic-polymath.outseta.com',
+                load: 'auth,customForm,emailList,leadCapture,nocode,profile,support',
+              };
+            `,
+          }}
+        />
+        <script
+          src="https://cdn.outseta.com/outseta.min.js"
+          data-options="o_options"
+          async
+        />
+        <script
+          src="https://auth.magic.link/sdk"
+          async
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+} 
