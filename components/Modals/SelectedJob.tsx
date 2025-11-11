7bdef174a5ecf86ef1694dda74db29dbe7f6f424
Toriong <gtorion97@gmail.com>
2025-11-10 17:02:51 -0800

    fixed eslint errors

--- components/Modals/SelectedJob.tsx
+++ components/Modals/SelectedJob.tsx
@@ -1,22 +1,22 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import Modal from "react-bootstrap/Modal";
-import { IoIosSchool } from "react-icons/io";
-import { BiTrendingUp } from "react-icons/bi";
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { IoIosSchool } from 'react-icons/io';
+import { BiTrendingUp } from 'react-icons/bi';
 import {
   MdOutlineTransferWithinAStation,
   MdOutlineDirectionsWalk,
   MdSupervisedUserCircle,
   MdAttachMoney,
-} from "react-icons/md";
-import { ISelectedJob, useModalContext } from "../../providers/ModalProvider";
-import jobVizDataObj from "../../data/Jobviz/jobVizDataObj.json";
-import { useRouter } from "next/router";
-import getNewPathsWhenModalCloses from "../../helperFns/getNewPathsWhenModalCloses";
-import { replaceCharAt } from "../../shared/fns";
-import CopyableTxt from "../CopyableTxt";
-import { toast } from "react-toastify";
-import { useSearchParams } from "next/navigation";
-import { createSelectedJobVizJobLink } from "../JobViz/JobTours/JobToursCard";
+} from 'react-icons/md';
+import { ISelectedJob, useModalContext } from '../../providers/ModalProvider';
+import jobVizDataObj from '../../data/Jobviz/jobVizDataObj.json';
+import { useRouter } from 'next/router';
+import getNewPathsWhenModalCloses from '../../helperFns/getNewPathsWhenModalCloses';
+import { replaceCharAt } from '../../shared/fns';
+import CopyableTxt from '../CopyableTxt';
+import { toast } from 'react-toastify';
+import { useSearchParams } from 'next/navigation';
+import { createSelectedJobVizJobLink } from '../JobViz/JobTours/JobToursCard';
 
 const { Header, Title, Body } = Modal;
 const { data_start_yr: _data_start_yr, data_end_yr: _data_end_yr } =
@@ -25,18 +25,18 @@ const DATA_START_YR = _data_start_yr[0];
 const DATA_END_YR = _data_end_yr[0];
 
 const formatToCurrency = (num: number) =>
-  num.toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD",
+  num.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
     maximumFractionDigits: 0,
   });
 
 const formatDataTxtToCurrency = (dataTxt: number | null) => {
-  if (typeof dataTxt === "number") {
+  if (typeof dataTxt === 'number') {
     return formatToCurrency(dataTxt);
   }
 
-  return "Data unavailable";
+  return 'Data unavailable';
 };
 
 const createInfoCards = (selectedJob: ISelectedJob | null) => {
@@ -54,41 +54,41 @@ const createInfoCards = (selectedJob: ISelectedJob | null) => {
   } = selectedJob;
   const onTheJobTraining =
     selectedJob[
-      "typical_on-the-job_training_needed_to_attain_competency_in_the_occupation"
+      'typical_on-the-job_training_needed_to_attain_competency_in_the_occupation'
     ];
   const infoCards = [
     {
-      id: "medianAnnualWage",
+      id: 'medianAnnualWage',
       title: `Median ${DATA_START_YR} Annual Wage`,
       txt: formatDataTxtToCurrency(median_annual_wage),
       icon: <MdAttachMoney />,
     },
     {
-      id: "educationNeededForEntry",
-      title: "Education Needed",
+      id: 'educationNeededForEntry',
+      title: 'Education Needed',
       txt: typical_education_needed_for_entry,
       icon: <IoIosSchool />,
     },
     {
-      id: "onTheJobTraining",
-      title: "On-the-job Training",
+      id: 'onTheJobTraining',
+      title: 'On-the-job Training',
       txt: onTheJobTraining,
       icon: <MdSupervisedUserCircle />,
     },
     {
-      id: "employmentStartYr",
+      id: 'employmentStartYr',
       title: `${DATA_START_YR} Employment`,
       txt: employment_start_yr.toLocaleString(),
       icon: <MdOutlineDirectionsWalk />,
     },
     {
-      id: "employmentEndYr",
+      id: 'employmentEndYr',
       title: `Predicted ${DATA_END_YR} Employment`,
       txt: employment_end_yr.toLocaleString(),
       icon: <MdOutlineTransferWithinAStation />,
     },
     {
-      id: "employmentChange",
+      id: 'employmentChange',
       title: `Predicted change in Employment ${DATA_START_YR} - ${DATA_END_YR}`,
       txt: employment_change_percent,
       icon: <BiTrendingUp />,
@@ -105,18 +105,18 @@ const InfoCards: React.FC<{
     const { icon, title, txt, id } = card;
     let _txt = txt;
 
-    if (typeof _txt === "string" && _txt.split(" ").length === 2) {
-      const txtSplitted = _txt.split(" ");
+    if (typeof _txt === 'string' && _txt.split(' ').length === 2) {
+      const txtSplitted = _txt.split(' ');
       let [firstWord, secondWord] = txtSplitted;
       const firstChar = firstWord.charAt(0);
       firstWord = replaceCharAt(firstWord, 0, firstChar.toUpperCase());
       _txt = `${firstWord} ${secondWord}`;
     }
 
-    if (id === "employmentChange" && typeof _txt === "number") {
-      _txt = `${Math.sign(_txt) ? "+" : ""}${_txt.toLocaleString()}%`;
-    } else if (id === "employmentChange") {
-      _txt = "Data unavailable";
+    if (id === 'employmentChange' && typeof _txt === 'number') {
+      _txt = `${Math.sign(_txt) ? '+' : ''}${_txt.toLocaleString()}%`;
+    } else if (id === 'employmentChange') {
+      _txt = 'Data unavailable';
     }
 
     return (
@@ -154,7 +154,15 @@ const CopyLinkTxtAndIcon: React.FC = () => {
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+        <rect
+          x="9"
+          y="9"
+          width="13"
+          height="13"
+          rx="2"
+          ry="2"
+        >
+        </rect>
         <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
       </svg>
     </>
@@ -167,15 +175,15 @@ const SelectedJob: React.FC = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [selectedJob, setSelectedJob] = _selectedJob;
-  const [jobLink, setJobLink] = useState("");
+  const [jobLink, setJobLink] = useState('');
   const [isJobModal, setIsJobModal] = _isJobModalOn;
   const [, setJobToursModalCssProps] = _jobToursModalCssProps;
   let { soc_title, def: _def, title, BLS_link } = selectedJob ?? {};
   let jobTitle = soc_title ?? title;
-  jobTitle = jobTitle === "Total, all" ? "All US Jobs" : jobTitle;
-  let def: string | null = _def ?? "";
+  jobTitle = jobTitle === 'Total, all' ? 'All US Jobs' : jobTitle;
+  let def: string | null = _def ?? '';
   def =
-    def.toLowerCase() === "no definition found for this summary category."
+    def.toLowerCase() === 'no definition found for this summary category.'
       ? null
       : def;
 
@@ -186,18 +194,18 @@ const SelectedJob: React.FC = () => {
   const infoCards = createInfoCards(selectedJob);
 
   const handleOnHide = () => {
-    const searchResults = router.query["search-results"];
+    const searchResults = router.query['search-results'];
 
-    if (router.asPath.includes("jobviz") && Array.isArray(searchResults)) {
+    if (router.asPath.includes('jobviz') && Array.isArray(searchResults)) {
       const newPaths = getNewPathsWhenModalCloses(
-        router.query["search-results"]
+        router.query['search-results']
       );
       const paramsStr = searchParams.toString();
       const url = paramsStr.length
         ? `${window.location.origin}/jobviz${newPaths}?${paramsStr}`
         : `${window.location.origin}/jobviz${newPaths}`;
 
-      console.log("Navigating to URL on modal close: ", url);
+      console.log('Navigating to URL on modal close: ', url);
 
       router.push(url, undefined, {
         scroll: false,
@@ -216,10 +224,10 @@ const SelectedJob: React.FC = () => {
       return;
     }
 
-    console.error("BLS_link is falsy. Cannot copy link.");
+    console.error('BLS_link is falsy. Cannot copy link.');
   };
 
-  const toastLinkCopied = useRef("");
+  const toastLinkCopied = useRef('');
 
   const handleCopyLinkBtnPressOnMobile = () => {
     if (toastLinkCopied.current) {
@@ -231,7 +239,7 @@ const SelectedJob: React.FC = () => {
       return;
     }
 
-    console.error("BLS_link is falsy. Cannot copy link.");
+    console.error('BLS_link is falsy. Cannot copy link.');
   };
 
   useEffect(() => {
@@ -254,7 +262,7 @@ const SelectedJob: React.FC = () => {
           return;
         }
 
-        console.error("selectedJob is falsy. Cannot create job link.");
+        console.error('selectedJob is falsy. Cannot create job link.');
       }}
       contentClassName="selectedJobModal"
       dialogClassName="dialogJobVizModal"
@@ -285,11 +293,11 @@ const SelectedJob: React.FC = () => {
         <section className="flex-column flex-sm-row d-flex align-items-center justify-content-between pt-2 mt-3 border-top">
           <div
             className={`d-flex align-items-center justify-content-center text-sm-start text-center ${
-              BLS_link ? "" : "pe-none invisible"
+              BLS_link ? '' : 'pe-none invisible'
             }`}
           >
             <span className="me-sm-2 ">
-              Learn More:{" "}
+              Learn More:{' '}
               <span
                 onClick={() => {
                   if (BLS_link) {
