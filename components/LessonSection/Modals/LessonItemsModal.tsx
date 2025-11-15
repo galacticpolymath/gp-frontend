9a8e7a28794bc01312386e86b3573602796995f4
Toriong <gtorion97@gmail.com>
2025-11-14 18:44:50 -0800

    fixing jobviz modal ui problem, not being center in relation to the viewport

--- components/LessonSection/Modals/LessonItemsModal.tsx
+++ components/LessonSection/Modals/LessonItemsModal.tsx
@@ -446,9 +446,40 @@ const LessonItemsModal: React.FC = () => {
                 isGpPlusMember
                   ? "col-12 col-sm-7 col-xxl-9"
                   : "col-6 col-sm-6 col-md-9 col-xxl-6 p-0"
-              } d-flex flex-column flex-md-row justify-content-md-end align-items-center p-sm-0`}
+              } d-flex flex-column flex-md-row justify-content-md-end align-items-sm-center p-sm-0`}
             >
-              <section className="w-100 d-flex flex-column flex-md-row justify-content-end align-items-stretch">
+              <section className="d-none d-sm-flex h-100 justify-content-end pe-sm-3">
+                <Button
+                  style={{
+                    backgroundColor: "transparent",
+                    transition: "background-color inifinte",
+                    width: "fit-content",
+                  }}
+                  className="mb-1 mb-md-0 d-flex no-btn-styles justify-content-center align-items-center underline-on-hover text-black pointer"
+                  onClick={handlePlayBtnClick}
+                >
+                  <section className="w-100 d-none d-sm-flex h-100 pt-1">
+                    <h6
+                      style={{
+                        height: "fit-content",
+                        textTransform: "none",
+                      }}
+                      className="mb-0 text-black d-flex d-md-block justify-content-center align-items-center h-100 text-nowrap me-2"
+                    >
+                      Play Now:
+                    </h6>
+                  </section>
+                  <section className="w-100 d-none d-sm-flex h-100">
+                    <div
+                      style={{ width: 44, height: 44 }}
+                      className="bg-white p-1 rounded"
+                    >
+                      <LuMonitorPlay color="black" size={35} />
+                    </div>
+                  </section>
+                </Button>
+              </section>
+              <section className="d-flex flex-column flex-md-row justify-content-end align-items-stretch">
                 {currentLessonItem.itemCat !== "web resource" &&
                   currentLessonItem.isExportable && (
                     <section className="w-100 d-none d-sm-flex justify-content-center justify-content-md-end pt-sm-1">
