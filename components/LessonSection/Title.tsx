1eaf69d4351eb5e26904794e30b9858c0cbd5b04
Toriong <gtorion97@gmail.com>
2025-11-21 20:56:04 -0800

    updated the sponsor text, rendering text as is

--- components/LessonSection/Title.tsx
+++ components/LessonSection/Title.tsx
@@ -163,11 +163,7 @@ const Title = (props: ITitleProps) => {
                 </button>
               )}
             {availLocs && (
-              <LocDropdown 
-                availLocs={availLocs} 
-                loc={locale} 
-                id={numID} 
-              />
+              <LocDropdown availLocs={availLocs} loc={locale} id={numID} />
             )}
           </div>
           <div className="d-flex flex-column flex-xxl-row">
@@ -201,15 +197,12 @@ const Title = (props: ITitleProps) => {
             </div>
             <div className="ms-xxl-3 mb-4 mb-md-3 mb-lg-0 mt-4 mt-md-0 col-xxl-3 row my-0 py-0 d-xxl-flex flex-xxl-column-reverse justify-content-xxl-center align-items-xxl-center">
               <div className="col-12 col-sm-8 col-md-8 col-lg-9 col-xxl-12 d-grid align-content-center d-xxl-flex flex-xxl-column p-0">
-                <h5 className="text-xxl-center">Sponsored by:</h5>
-                <ul className="ps-xxl-4 d-xxl-flex flex-xxl-column justify-content-xxl-center align-items-xxl-center m-xxl-0">
-                  {!!sponsors?.length &&
-                    sponsors.map((sponsor, index) => (
-                      <li className="text-xxl-center" key={index}>
-                        <RichText content={sponsor} />
-                      </li>
-                    ))}
-                </ul>
+                <h5 className="text-center">Sponsored by:</h5>
+                <div className="text-center">
+                  {SponsoredBy && typeof SponsoredBy === "string" && (
+                    <RichText content={SponsoredBy} />
+                  )}
+                </div>
               </div>
               <div className="col-5 col-sm-4  col-md-3 col-lg-3 col-xxl-12 m-auto d-grid m-xxl-0">
                 {sponsorLogoImgUrl && (
